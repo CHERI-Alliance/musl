f020c4121c69bfe5d067ead3420ae177b81b2945
Jack Bond-Preston <jack.bond-preston@arm.com>
2021-07-07 11:10:53 +0100

    Morello: Improve open and creat tests
    Make testing for open a bit more complete now mktemp is ported, and
    add a test for creat function.
    
    Change-Id: I7d3f86f06cd7841ac5cc7cf471fa86e3066de5a4

--- test/src/open.c
+++ test/src/open.c
@@ -3,28 +3,48 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+#define TMP_DIR "/tmp/morello-musl-tests-open/"
+#define IN_TMP_DIR(f) TMP_DIR f
+
+mode_t mode = 0700;
 
 int test_open() {
+  // set up root dir in /tmp
+  if (mkdir(TMP_DIR, 0700) && errno != EEXIST) return -2;
+
   // open temp file
   int fd = open(".", O_TMPFILE | O_RDWR | O_EXCL, mode);
-  if (fd < 0) {
-    return 2;
-  }
+  if (fd < 0) return 1;
+  if (close(fd)) return -3;
 
   // open our own exe (read only)
-  int fd2 = open("/proc/self/exe", O_RDONLY, mode);
-  if (fd2 < 0) {
-    return 3;
-  }
+  fd = open("/proc/self/exe", O_RDONLY, mode);
+  if (fd < 0) return 2;
+  if (close(fd)) return -3;
 
-  if (close(fd) || close(fd2)) {
-    return 1;
-  }
+  // generate name for file in /tmp/...
+  char template[] = IN_TMP_DIR("openXXXXXX");
+  char *filename;
+  filename = mktemp(template);
 
-  // TODO: pending mktemp(), use open() to create named file,
-  // and then try to create again with O_EXCL and check it fails
+  // create file with generated name
+  fd = open(filename, O_CREAT, mode);
+  if (fd < 0) return 3;
+  if (close(fd)) return -3;
+
+  // try to create same file with O_EXCL
+  fd = open(filename, O_CREAT | O_EXCL, mode);
+  if (fd >= 0) return 4;
+
+  // open same file again, and unlink to delete
+  fd = open(filename, O_RDWR, mode);
+  if (unlink(filename)) return -4;
+  if (fd < 0) return 5;
+  if (close(fd)) return -3;
 
   return 0;
 }
@@ -34,28 +54,29 @@ int test_openat() {
 
   // try to open ourself (read-only)
   int fd = openat(dirfd, "exe", O_RDONLY);
-  if (fd < 0) {
-    return 4;
-  }
-
-  if (close(fd)) {
-    return 1;
-  }
+  if (fd < 0) return 1;
+  if (close(fd)) return -3;
 
   return 0;
 }
 
+int test_creat() {
+  // set up root dir in /tmp
+  if (mkdir(TMP_DIR, 0700) && errno != EEXIST) return -2;
 
-// TODO: pending mktemp()
+  // generate name for file in /tmp/...
+  char template[] = IN_TMP_DIR("openXXXXXX");
+  char *filename;
+  filename = mktemp(template);
 
-// creat() is just a wrapper around open()
-// int test_creat() {
-//   int fd = creat("tmpfile", mode);
-//   if (fd < 0) {
-//     return 5;
-//   }
-//   return 0;
-// }
+  int fd = creat(filename, mode);
+
+  if (unlink(filename)) return -4;
+  if (fd < 0) return 1;
+  if (close(fd)) return -3;
+
+  return 0;
+}
 
 int main(int argc, char **argv) {
   switch (argv[1][0]) {
@@ -63,8 +84,8 @@ int main(int argc, char **argv) {
       return test_open();
     case '1': // openat
       return test_openat();
-    // case '2': // creat
-    //   return test_creat();
+    case '2': // creat
+      return test_creat();
   }
 
   return -1;

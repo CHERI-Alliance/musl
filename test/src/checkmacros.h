fe7528524e2c533479830dac231a373d1d9a639c
Jack Bond-Preston <jack.bond-preston@arm.com>
2021-11-16 17:10:23 +0000

    Remove DDC from checkmacros.h
    Change-Id: I814305365f7ef1e49bcd049b6d84a0c9d1e15982

--- test/src/checkmacros.h
+++ test/src/checkmacros.h
@@ -2,6 +2,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #define COMPARE_BYTES(_q, _p, n) ({ \
 	char *pb = (char *)_p;          \
@@ -16,26 +17,20 @@
 })
 
 /* Get memory tag for aligned address */
-static inline uint64_t get_mem_tag(ptraddr_t address)
+static inline bool get_mem_tag(ptraddr_t address, void *wide_cap)
 {
-	uint64_t res;
-	__asm__ volatile (
-		"mrs      c1, DDC\n"
-		"scvalue  c1, c1, %1\n"
-		"ldr      c1, [c1]\n"
-		"gctag    %0, c1\n"
-		: "=r"(res) : "r"(address));
-	return res;
+	void **cap = __builtin_cheri_address_set(wide_cap, address);
+	return __builtin_cheri_tag_get(*cap);
 }
 
 #define CHECK_MEM_TAGS(_q, _p, n) ({                            \
 	int u = 0;                                                  \
-	for(size_t k = 0; k < n && n >=16; k++) {                   \
+	for(size_t k = 0; k < n && n >= 16 && (n - k) >= 16; k++) { \
 		ptraddr_t  x = (uintptr_t )_p + k;                      \
 		ptraddr_t  y = (uintptr_t )_q + k;                      \
 		if ((x & 0xf) == 0 && (y & 0xf) == 0) {                 \
-			uint64_t tx = get_mem_tag(x);                       \
-			uint64_t ty = get_mem_tag(y);                       \
+			uint64_t tx = get_mem_tag(x, _p);                   \
+			uint64_t ty = get_mem_tag(y, _q);                   \
 			if (tx != ty) {                                     \
 				printf("tag: %016lx %016lx - %lu %lu\n",        \
 					y, x, ty, tx);                              \

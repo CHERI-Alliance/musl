11341ff29aeb8ff11045d0081373765a61107a43
Werner Lewis <werner.lewis@arm.com>
2022-01-10 14:55:49 +0000

    Morello: Add various functions
     - Add termios functions to Morello objects.
     - Add multibyte functions to Morello objects
       Tests are added for functionality of char16_t
       and mb string conversion.
     - Add network functions to Morello objects:
       ether, proto and inet functions tested and
       added to morello.objects.
     - Various trivial ports are added to Morello
       objects after testing for correct functionality.
     - Add alignment test for wmemset.
     - Enable additional tests for printf variants.
    
    Change-Id: I4190bd798d586fd9ef5097db16481def3e5a09cf

--- test/src/mbrtowc.c
+++ test/src/mbrtowc.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <uchar.h>
 #include <wchar.h>
 #include <locale.h>
 
-int check_result(size_t len, size_t expected_len, wchar_t* wcp, mbstate_t* stp)
+int check_result(size_t len, size_t expected_len, void* wcp, mbstate_t* stp)
 {
     unsigned return_value = 0;
     if (len == (size_t) -1) {
@@ -33,13 +34,12 @@ int check_result(size_t len, size_t expected_len, wchar_t* wcp, mbstate_t* stp)
     return return_value;
 }
 
-int main(void) {
-    setlocale(LC_ALL, "en_GB.utf8");
-
+int test_mbrtowc(void)
+{
     wchar_t wc = 0;
-	mbstate_t st = (mbstate_t){0};
+    mbstate_t st = (mbstate_t){0};
     wchar_t* wcp = &wc;
-	mbstate_t* stp = &st;
+    mbstate_t* stp = &st;
     const char* src = "z\u00df\u6c34\U0001d10b"; // or u8"zß水𝄋"
                       // or "\x7a\xc3\x9f\xe6\xb0\xb4\xf0\x9d\x84\x8b";
     unsigned int remaining_char = 11;
@@ -47,20 +47,83 @@ int main(void) {
     size_t len;
 
     len = mbrtowc(wcp, src, remaining_char, stp);
-    if ((return_value = check_result(len,1,wcp,stp))) return return_value;
+    if ((return_value = check_result(len,1,(void*)wcp,stp))) return return_value;
     src += len, remaining_char -= len;
 
     len = mbrtowc(wcp, src, remaining_char, stp);
-    if ((return_value = check_result(len,2,wcp,stp))) return return_value;
+    if ((return_value = check_result(len,2,(void*)wcp,stp))) return return_value;
     src += len, remaining_char -= len;
 
     len = mbrtowc(wcp, src, remaining_char, stp);
-    if ((return_value = check_result(len,3,wcp,stp))) return return_value;
+    if ((return_value = check_result(len,3,(void*)wcp,stp))) return return_value;
     src += len, remaining_char -= len;
 
     len = mbrtowc(wcp, src, remaining_char, stp);
-    if ((return_value = check_result(len,4,wcp,stp))) return return_value;
+    if ((return_value = check_result(len,4,(void*)wcp,stp))) return return_value;
     src += len, remaining_char -= len;
 
     return 0;
+}
+
+int test_mbtoc16(void) {
+    mbstate_t st = (mbstate_t){0};
+    char16_t c16p[2];
+    mbstate_t* stp = &st;
+    const char* src = "z\u00df\u6c34\U0001d10b"; // or u8"zß水𝄋"
+                      // or "\x7a\xc3\x9f\xe6\xb0\xb4\xf0\x9d\x84\x8b";
+    unsigned int remaining_char = 11;
+    int return_value = 0;
+    size_t len;
+
+    len = mbrtoc16(c16p, src, remaining_char, stp);
+    if ((return_value = check_result(len,1,(void*)c16p,stp))) return return_value;
+    src += len, remaining_char -= len;
+
+    len = mbrtoc16(c16p, src, remaining_char, stp);
+    if ((return_value = check_result(len,2,(void*)c16p,stp))) return return_value;
+    src += len, remaining_char -= len;
+
+    len = mbrtoc16(c16p, src, remaining_char, stp);
+    if (*(int*)stp && mbrtoc16(&c16p[1], src, remaining_char, stp) != -3) return 16;
+    if ((return_value = check_result(len,3,(void*)c16p,stp))) return return_value;
+    src += len, remaining_char -= len;
+
+    len = mbrtoc16(c16p, src, remaining_char, stp);
+    if (*(int*)stp && mbrtoc16(&c16p[1], src, remaining_char, stp) != -3) return 16;
+    if ((return_value = check_result(len,4,(void*)c16p,stp))) return return_value;
+    src += len, remaining_char -= len;
+
+    return 0;
+}
+
+int test_mbsnrtowcs(void) {
+    mbstate_t st = (mbstate_t){0};
+    wchar_t wcp[10];
+    mbstate_t* stp = &st;
+    const char* src = "z\u00df\u6c34\U0001d10b"; // or u8"zß水𝄋"
+                      // or "\x7a\xc3\x9f\xe6\xb0\xb4\xf0\x9d\x84\x8b";
+    size_t expected_len = 4;
+    int return_value = 0;
+    size_t len;
+
+    len = mbsnrtowcs(wcp, &src, 11, expected_len, stp);
+    if ((return_value = check_result(len,expected_len,(void*)wcp,stp))) return return_value;
+    if (len != expected_len) return 16;
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    setlocale(LC_ALL, "en_GB.utf8");
+
+    if (argc < 2) return 1;
+    switch (argv[1][0]) {
+    case '0':
+        return test_mbrtowc();
+    case '1':
+        return test_mbtoc16();
+    case '2':
+        return test_mbsnrtowcs();
+    default:
+        return 2;
+    }
 }

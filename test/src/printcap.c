7d205f853de4a23934a8b57d6cec560fc8ef111c
Tamas Petz <tamas.petz@arm.com>
2022-02-01 19:39:16 +0100

    Morello: Make Morello tests CHERI-generic
    Change-Id: Ie94b1fc6675660f115796f6026c540f677b47ddd

--- test/src/printcap.c
+++ test/src/printcap.c
@@ -25,8 +25,7 @@ static int test_cap_max()
 {
 	/* We make _almost_ cap max because we can't forge tag for it */
 	uint64_t hi = 0xffffc00000010005ul;
-	void *max = NULL;
-	__asm__ volatile ("cthi %0, %0, %1" : "+C"(max) : "r"(hi));
+	void *max = __builtin_cheri_copy_to_high(NULL, hi);
 	int n = printf("%#p\n", max);
 	return n > 0 ? 0 : 2;
 }
@@ -34,20 +33,21 @@ static int test_cap_max()
 static int test_cap_sealed()
 {
 	void *fun;
+#ifdef MORELLO
 	__asm__ volatile ("adrp    %0, test_cap_sealed" : "+C"(fun));
 	__asm__ volatile ("add     %0, %0, :lo12:test_cap_sealed" : "+C"(fun));
 	__asm__ volatile ("seal %0, %0, LPB" : "+C"(fun));
+#else
+	// This is not LPB but RB.
+	fun = (void*)__builtin_cheri_seal_entry(fun);
+#endif
 	int n = printf("%#p\n", fun);
 	return n > 0 ? 0 : 2;
 }
 
 static int test_cap_sentry()
 {
-	void *fun;
-	__asm__ volatile ("adrp    %0, test_cap_sentry" : "+C"(fun));
-	__asm__ volatile ("add     %0, %0, :lo12:test_cap_sentry" : "+C"(fun));
-	__asm__ volatile ("seal %0, %0, RB" : "+C"(fun));
-	int n = printf("%#p\n", fun);
+	int n = printf("%#p\n", (void *)test_cap_sentry);
 	return n > 0 ? 0 : 2;
 }
 
@@ -55,7 +55,12 @@ static int test_cap_sealed_invalid()
 {
 	void *fun = (void *)test_cap_sealed;
 	fun = __builtin_cheri_tag_clear(fun);
+#ifdef MORELLO
 	__asm__ volatile ("seal %0, %0, LPB" : "+C"(fun));
+#else
+	// This is not LPB but RB.
+	fun = (void*)__builtin_cheri_seal_entry(fun);
+#endif
 	int n = printf("%#p\n", fun);
 	return n > 0 ? 0 : 2;
 }

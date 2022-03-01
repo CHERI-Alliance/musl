fec671be84494a10a9ffdaf80cc1898e506763a8
Jack Bond-Preston <jack.bond-preston@arm.com>
2022-03-01 09:52:23 +0000

    Add tests for various functions
    Change-Id: Ia3abd285ac667f075c641f72009c97687cec04bd

--- test/src/stdio_misc.c
+++ test/src/stdio_misc.c
@@ -7,6 +7,8 @@
 int test_perror();
 int test_ctermid();
 int test_fopencookie();
+int test_setvbuf();
+int test_getdelim();
 
 int main(int argc, char **argv) {
     if (argc < 2) return -1;
@@ -18,9 +20,45 @@ int main(int argc, char **argv) {
             return test_ctermid();
         case '2': // fopencookie
             return test_fopencookie();
+        case '3': // setvbuf
+            return test_setvbuf();
+        case '4': // getdelim
+            return test_getdelim();
     }
 
     return -1;
+}
+
+int test_getdelim() {
+    FILE* fp = tmpfile();
+    if(!fp) return 1;
+
+    if (fprintf(fp, "test|delimited|string") != 21) return 2;
+    rewind(fp);
+
+    char *str1 = NULL;
+    size_t n = 0;
+    if (getdelim(&str1, &n, '|', fp) != 5) return 3;
+    if (strcmp("test|", str1)) return 4;
+
+    char *str2 = malloc(16);
+    n = 16;
+    if (getdelim((char **) &str2, &n, '|', fp) != 10) return 5;
+    if (strcmp("delimited|", str2)) return 6;
+
+    return 0;
+}
+
+int test_setvbuf() {
+    FILE* fp = tmpfile();
+    if(!fp) return 1;
+
+    if (setvbuf(fp, NULL, _IOFBF, 64)) return 2;
+    while(fgetc(fp) != EOF); // read whole file
+
+    if (fclose(fp)) return 3;
+
+    return 0;
 }
 
 int test_perror() {

4d2f2c61708cf350236816ff0b8d9bba4906868f
Tamas Petz <tamas.petz@arm.com>
2022-03-16 12:47:05 +0100

    Morello: Add more pthread_cancel tests
    These tests now check the following cases:
      1. Async cancellation when a thread is not at a
         cancellation point at the time of a cancel.
      2. Async cancellation when a thread is at a
         cancellation point at the time of a cancel.
      3. Deferred cancellation when a thread is not at a
         cancellation point at the time of a cancel.
      4. Deferred cancellation when a thread is at a
         cancellation point at the time of a cancel.
      5. Async cancellation when cancellation is disabled.
      6. Deferred cancellation when cancellation is disabled.
      7. [extension] Async cancellation with cancellation
         masked.
      8. [extension] Deferred cancellation with cancellation
         masked.
    
    Change-Id: I93cfd6dd8559a4bae12909b52c1b962c66db250e

--- src/thread/morello/syscall_cp_libshim.c
+++ src/thread/morello/syscall_cp_libshim.c
@@ -13,42 +13,42 @@
 #include "pthread_impl.h"
 
 intptr_t __syscall_cp_asm(volatile void *cp, syscall_arg_t nr,
-                      syscall_arg_t u, syscall_arg_t v, syscall_arg_t w,
-                      syscall_arg_t x, syscall_arg_t y, syscall_arg_t z) {
-    volatile char* cancel = cp;
-    if (*cancel) {
-        goto __cp_cancel;
-    }
+					  syscall_arg_t u, syscall_arg_t v, syscall_arg_t w,
+					  syscall_arg_t x, syscall_arg_t y, syscall_arg_t z) {
+	volatile char* cancel = cp;
+	if (*cancel) {
+		goto __cp_cancel;
+	}
 
-    pthread_t self = __pthread_self();
-    self->in_syscall_cp = 1;
+	pthread_t self = __pthread_self();
+	self->in_syscall_cp = 1;
 
-    __asm__ volatile(
-        ".global __cp_begin\n" \
-        ".hidden __cp_begin\n" \
-        ".size __cp_begin, 1\n" \
-        "__cp_begin:":
-    );
-    intptr_t ret = syscall(nr, u, v, w, x, y, z);
+	__asm__ volatile(
+		".global __cp_begin\n" \
+		".hidden __cp_begin\n" \
+		".size __cp_begin, 1\n" \
+		"__cp_begin:":
+	);
+	intptr_t ret = syscall(nr, u, v, w, x, y, z);
 
-    __asm__ volatile(
-        ".global __cp_end\n" \
-        ".hidden __cp_end\n" \
-        ".size __cp_end, 1\n" \
-        "__cp_end:":
-    );
-    self->in_syscall_cp = 0;
+	__asm__ volatile(
+		".global __cp_end\n" \
+		".hidden __cp_end\n" \
+		".size __cp_end, 1\n" \
+		"__cp_end:":
+	);
+	self->in_syscall_cp = 0;
 
-    return ret;
+	return ret;
 
 __cp_cancel:
-    __asm__ volatile(
-        ".global __cp_cancel\n" \
-        ".hidden __cp_cancel\n" \
-        ".size __cp_cancel, 1\n" \
-        "__cp_cancel:\n" \
-        "b __cancel\n":
-    );
+	__asm__ volatile(
+		".global __cp_cancel\n" \
+		".hidden __cp_cancel\n" \
+		".size __cp_cancel, 1\n" \
+		"__cp_cancel:\n" \
+		"b __cancel\n":
+	);
 }
 
 #endif

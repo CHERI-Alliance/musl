37eda9dbc28bd542ef37604bd7cf7ac67a2d45ac
Tudor Cretu <tudor.cretu@arm.com>
2022-04-01 19:01:14 +0100

    Morello: Fix stpncpy
    Change-Id: I8302b6e363ae8001cc4966f337e00cb41051c206

--- src/string/stpncpy.c
+++ src/string/stpncpy.c
@@ -1,7 +1,6 @@
 #include <string.h>
 #include <stdint.h>
 #include <limits.h>
-#include <stdbool.h>
 
 #include "morello_helpers.h"
 
@@ -13,23 +12,16 @@
 char *__stpncpy(char *restrict d, const char *restrict s, size_t n)
 {
 #ifdef __GNUC__
-	size_t i = 0;
-	size_t max_i = CAP_TAIL_LENGTH(s);
-
 	typedef size_t __attribute__((__may_alias__)) word;
 	word *wd;
 	const word *ws;
 	if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN)) {
-		for (; ((uintptr_t)s & ALIGN) && n && (*d=*s); n--, s++, d++, i++);
+		for (; ((uintptr_t)s & ALIGN) && n && (*d=*s); n--, s++, d++);
 		if (!n || !*s) goto tail;
-		if (LT_IF_MORELLO_ELSE(i + sizeof(word) - 1, max_i, true)) {
-			wd=(void *)d; ws=(const void *)s;
-                        for (; LT_IF_MORELLO_ELSE(i + sizeof(word) - 1, max_i, true) &&
-                               n >= sizeof(size_t) && !HASZERO(*ws);
-                             n -= sizeof(size_t), ws++, wd++)
-                          *wd = *ws;
-                        d=(void *)wd; s=(const void *)ws;
-		}
+		wd=(void *)d; ws=(const void *)s;
+		for (; CAP_TAIL_LENGTH(ws) >= sizeof(word) && n>=sizeof(size_t) && !HASZERO(*ws);
+		       n-=sizeof(size_t), ws++, wd++) *wd = *ws;
+		d=(void *)wd; s=(const void *)ws;
 	}
 #endif
 	for (; n && (*d=*s); n--, s++, d++);

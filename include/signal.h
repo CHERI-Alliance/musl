7d3314877bde50167b218eff518afed7ca36e76d
Zachary Leaf <zachary.leaf@arm.com>
2022-08-01 12:41:17 +0100

    Morello: fix alignment of siginfo_t struct members
    In Morello, where pointers are stored as 128 bit capabilities, the
    siginfo_t->__sigchld struct member offset (0x20) is different to that
    produced by linux/include/uapi/asm-generic/siginfo.h (0x18).
    
    This is caused by the musl definition including sigval in a union with
    __sigchld. As sigval may contain a ptr, it is therefore aligned on ptr
    boundaries. In the case of 16B caps/ptrs, this causes __sigchld to also
    be aligned at a 16B boundary. This behaviour is not consistent with the
    kernel uapi definition.
    
    Remove problematic unions from musl definition to ensure offsets are
    consistent with kernel uapi.
    
    Change-Id: I44ca80383300846d5ff67eb9777087412a00057e

--- include/signal.h
+++ include/signal.h
@@ -103,24 +103,26 @@ typedef struct {
 	union {
 		char __pad[128 - 2*sizeof(int) - sizeof(long)];
 		struct {
-			union {
-				struct {
-					pid_t si_pid;
-					uid_t si_uid;
-				} __piduid;
-				struct {
-					int si_timerid;
-					int si_overrun;
-				} __timer;
-			} __first;
-			union {
-				union sigval si_value;
-				struct {
-					int si_status;
-					clock_t si_utime, si_stime;
-				} __sigchld;
-			} __second;
-		} __si_common;
+			pid_t si_pid;
+			uid_t si_uid;
+		} __kill;
+		struct {
+			int si_timerid;
+			int si_overrun;
+			union sigval _sigval;
+		} __timer;
+		struct {
+			pid_t si_pid;
+			uid_t si_uid;
+			union sigval _sigval;
+		} __rt;
+		struct {
+			pid_t si_pid;
+			uid_t si_uid;
+			int si_status;
+			clock_t si_utime;
+			clock_t si_stime;
+		} __sigchld;
 		struct {
 			void *si_addr;
 			short si_addr_lsb;
@@ -143,12 +145,12 @@ typedef struct {
 		} __sigsys;
 	} __si_fields;
 } siginfo_t;
-#define si_pid     __si_fields.__si_common.__first.__piduid.si_pid
-#define si_uid     __si_fields.__si_common.__first.__piduid.si_uid
-#define si_status  __si_fields.__si_common.__second.__sigchld.si_status
-#define si_utime   __si_fields.__si_common.__second.__sigchld.si_utime
-#define si_stime   __si_fields.__si_common.__second.__sigchld.si_stime
-#define si_value   __si_fields.__si_common.__second.si_value
+#define si_pid     __si_fields.__kill.si_pid
+#define si_uid     __si_fields.__kill.si_uid
+#define si_status  __si_fields.__sigchld.si_status
+#define si_utime   __si_fields.__sigchld.si_utime
+#define si_stime   __si_fields.__sigchld.si_stime
+#define si_value   __si_fields.__rt._sigval
 #define si_addr    __si_fields.__sigfault.si_addr
 #define si_addr_lsb __si_fields.__sigfault.si_addr_lsb
 #define si_lower   __si_fields.__sigfault.__first.__addr_bnd.si_lower
@@ -156,8 +158,8 @@ typedef struct {
 #define si_pkey    __si_fields.__sigfault.__first.si_pkey
 #define si_band    __si_fields.__sigpoll.si_band
 #define si_fd      __si_fields.__sigpoll.si_fd
-#define si_timerid __si_fields.__si_common.__first.__timer.si_timerid
-#define si_overrun __si_fields.__si_common.__first.__timer.si_overrun
+#define si_timerid __si_fields.__timer.si_timerid
+#define si_overrun __si_fields.__timer.si_overrun
 #define si_ptr     si_value.sival_ptr
 #define si_int     si_value.sival_int
 #define si_call_addr __si_fields.__sigsys.si_call_addr

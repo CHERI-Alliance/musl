56a16ad2aa68d97fa068fc9cf5caf69c5676553c
Oliver Swede <oli.swede@arm.com>
2022-08-18 10:34:04 +0100

    Morello: Fix AT_PHDR and AT_BASE access
    The AT_PHDR and AT_BASE auxv elements are values (addresses),
    not capabilities.
    
    Change-Id: I576ee6520ac19e6462af8617cf27c39f4c1a0527

--- src/misc/getauxptr.c
+++ src/misc/getauxptr.c
@@ -12,8 +12,6 @@ void *__getauxptr(unsigned long item)
 	//  entries.
 	switch (item) {
 		case AT_ENTRY:
-		case AT_PHDR:
-		case AT_BASE:
 		case AT_SYSINFO_EHDR:
 		case AT_EXECFN:
 		case AT_RANDOM:

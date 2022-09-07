2be305f11e37d86f43c9f5dc13c5f2124ffc1286
Sherwin da Cruz <sherwin.dacruz@arm.com>
2022-09-07 14:31:15 +0000

    Morello: Add ERESERVATION error code
    Change-Id: I39773c4d83e01107e2dfdd342b7c2f18b5d13e11

--- arch/generic/bits/errno.h
+++ arch/generic/bits/errno.h
@@ -132,3 +132,7 @@
 #define ENOTRECOVERABLE 131
 #define ERFKILL         132
 #define EHWPOISON       133
+
+#ifndef ERESERVATION
+#define ERESERVATION    192
+#endif
--- src/errno/__strerror.h
+++ src/errno/__strerror.h
@@ -102,3 +102,5 @@ E(EDQUOT,       "Quota exceeded")
 E(ENOMEDIUM,    "No medium found")
 E(EMEDIUMTYPE,  "Wrong medium type")
 E(EMULTIHOP,    "Multihop attempted")
+
+E(ERESERVATION, "Bad memory reservation")

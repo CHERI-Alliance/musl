83b4d9049733fd7b1a4f6f1dd3d378bc6066806d
Yury Khrustalev <yury.khrustalev@arm.com>
2022-10-21 12:12:04 +0100

    Morello: Update FVP test runner
    Make changes to FVP-related tests required for a
    more portable execution of tests on Morello FVP.
    
    Change-Id: If478d481400f9d6fcec21c348c698ce130211bb0

--- test/src/printf_plain_text.c
+++ test/src/printf_plain_text.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char **argv)
 {
-    printf("This plain text with a new line used to do a thunk when built in non optimized mode.\nFor instanse, using -Og will do.\n");
-
+    printf("This plain text with a new line used to do a thunk when built in non optimized mode.\nFor instance, using -Og will do.\n");
     return 0;
 }

84be025cd6dc38ff9539d8055b749f76b0045c63
Yury Khrustalev <yury.khrustalev@arm.com>
2022-11-11 12:04:09 +0000

    Morello: [test] Update cap permissions in the auxv test
    Change-Id: I52760feda2b9c0d2838eca406585947a32a5dd94

--- test/src/cheri_test_helpers.h
+++ test/src/cheri_test_helpers.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <stdint.h>
+
+#define __CHERI_CAP_PERMISSION_USER0__	(1u << 2u)
+#define __CHERI_CAP_PERMISSION_USER1__	(1u << 3u)
+#define __CHERI_CAP_PERMISSION_USER2__	(1u << 4u)
+#define __CHERI_CAP_PERMISSION_USER3__	(1u << 5u)
+#define __CHERI_CAP_PERMISSION_VMEM__	__CHERI_CAP_PERMISSION_USER0__
+
 static const unsigned long READ_CAP_PERMS =
 #ifdef __ARM_CAP_PERMISSION_MUTABLE_LOAD__
 							__ARM_CAP_PERMISSION_MUTABLE_LOAD__ |
@@ -20,12 +28,44 @@ static unsigned long EXEC_CAP_PERMS =
 							__CHERI_CAP_PERMISSION_ACCESS_SYSTEM_REGISTERS__;
 
 static unsigned long ROOT_CAP_PERMS =
-#ifdef __ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__
-							__ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__ |
-#endif
+							__CHERI_CAP_PERMISSION_VMEM__ |
 							__CHERI_CAP_PERMISSION_GLOBAL__;
 
 static unsigned long SEAL_CAP_PERMS =
 							__CHERI_CAP_PERMISSION_GLOBAL__ |
 							__CHERI_CAP_PERMISSION_PERMIT_SEAL__ |
 							__CHERI_CAP_PERMISSION_PERMIT_UNSEAL__;
+
+#define TEST(mask, var) (((mask) & (var)) != 0)
+
+inline static const char *
+__print_perms(uint64_t perms, char *__perms_buffer)
+{
+	static uint64_t __permissions[] = {
+		__CHERI_CAP_PERMISSION_GLOBAL__,
+		__CHERI_CAP_PERMISSION_PERMIT_LOAD__,
+		__CHERI_CAP_PERMISSION_PERMIT_LOAD_CAPABILITY__,
+		__ARM_CAP_PERMISSION_MUTABLE_LOAD__,
+		__CHERI_CAP_PERMISSION_PERMIT_STORE__,
+		__CHERI_CAP_PERMISSION_PERMIT_STORE_CAPABILITY__,
+		__CHERI_CAP_PERMISSION_PERMIT_STORE_LOCAL__,
+		__CHERI_CAP_PERMISSION_PERMIT_EXECUTE__,
+		__ARM_CAP_PERMISSION_EXECUTIVE__,
+		__CHERI_CAP_PERMISSION_ACCESS_SYSTEM_REGISTERS__,
+		__CHERI_CAP_PERMISSION_PERMIT_SEAL__,
+		__CHERI_CAP_PERMISSION_PERMIT_UNSEAL__,
+		__ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__,
+		__ARM_CAP_PERMISSION_COMPARTMENT_ID__,
+		__CHERI_CAP_PERMISSION_VMEM__,
+		__CHERI_CAP_PERMISSION_USER1__,
+		__CHERI_CAP_PERMISSION_USER2__,
+		__CHERI_CAP_PERMISSION_USER3__
+	};
+	static char __permission_names[] = {'G', 'r', 'R', 'M', 'w', 'W', 'L', 'x', 'X', 'S', 's', 'u', 'B', 'C', 'V', '1', '2', '3'};
+	static int q = sizeof(__permissions) / sizeof(__permissions[0]);
+	for (int n = 0; n < q; n++) {
+		int has = TEST(__permissions[n], perms);
+		snprintf(__perms_buffer + n, 2, "%c", has ? __permission_names[n] : '-');
+	}
+	return __perms_buffer;
+}
--- test/src/getauxptrcheri.c
+++ test/src/getauxptrcheri.c
@@ -5,15 +5,13 @@
 
 #define RW_PERMS    (READ_CAP_PERMS | WRITE_CAP_PERMS | ROOT_CAP_PERMS)
 #define RX_PERMS    (READ_CAP_PERMS | EXEC_CAP_PERMS | ROOT_CAP_PERMS)
-#define CMPT_ID_PERMS    (__ARM_CAP_PERMISSION_COMPARTMENT_ID__)
+#define CMPT_ID_PERMS    (__CHERI_CAP_PERMISSION_GLOBAL__ | __ARM_CAP_PERMISSION_COMPARTMENT_ID__)
 
-typedef unsigned long uint;
-
-static int check(void *cap, uint req_perms, const char name[]);
+static int check(void *cap, uint64_t req_perms, const char name[]);
 
 int main (void) {
 
-	uint items[] = {
+	uint64_t items[] = {
 		AT_CHERI_EXEC_RW_CAP,
 		AT_CHERI_EXEC_RX_CAP,
 #ifdef DYNAMIC
@@ -25,11 +23,23 @@ int main (void) {
 		AT_CHERI_CID_CAP
 	};
 
-	uint req_perms[] = {
+	uint64_t req_perms[] = {
+#ifdef __ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__
+		__ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__ |
+#endif
 		RW_PERMS,
+#ifdef __ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__
+		__ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__ |
+#endif
 		RX_PERMS,
 #ifdef DYNAMIC
+#ifdef __ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__
+		__ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__ |
+#endif
 		RW_PERMS,
+#ifdef __ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__
+		__ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__ |
+#endif
 		RX_PERMS,
 #endif
 		RW_PERMS,
@@ -49,27 +59,34 @@ int main (void) {
 		"AT_CHERI_CID_CAP"
 	};
 
-	/* todo: remove -1 when AT_CHERI_CID_CAP is provided by libshim */
-	int n = sizeof(items) / sizeof(uint) - 1;
+	int n = sizeof(items) / sizeof(uint64_t);
 
+	int success = 1;
 	for (int k = 0; k < n; k++) {
 		void *cap = getauxptr(items[k]);
 		if (check(cap, req_perms[k], names[k])) {
-			return k + 1;
+			printf("root cap %s is not correct\n", names[k]);
+			success = 0;
 		}
 	}
 
-	return 0;
+	return success ? 0 : 1;
 }
 
-static int check(void *cap, uint req_perms, const char name[])
+static int check(void *cap, uint64_t req_perms, const char name[])
 {
 	if (!__builtin_cheri_tag_get(cap)) {
 		printf("tag is not set for %s: %#p\n", name, cap);
 		return 1;
 	}
-	if (__builtin_cheri_perms_get(cap) != req_perms) {
-		printf("perms are not correct for %s: %#p\n", name, cap);
+	uint64_t actual = __builtin_cheri_perms_get(cap);
+	if (actual != req_perms) {
+		char __perms_buffer1[19];
+		char __perms_buffer2[19];
+		printf("%s perms: actual: %s required: %s\n",
+			name,
+			__print_perms(actual, __perms_buffer1),
+			__print_perms(req_perms, __perms_buffer2));
 		return 2;
 	}
 	if (__builtin_cheri_sealed_get(cap)) {

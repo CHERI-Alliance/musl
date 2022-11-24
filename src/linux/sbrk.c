26b4226b165d5d5cb7337c49540294c263945644
Teo Couprie Diaz <teo.coupriediaz@arm.com>
2022-11-24 11:44:21 +0000

    sbrk: Revert morello related changes
    sbrk is not a system call but a wrapper around brk. In Musl, it is
    explicitly disabled for any argument other than 0. It allows for getting
    the current break but not changing it.
    
    In pure-cap brk is disabled, so the upstream version of sbrk will fail
    correcly as well. In Aarch64, it will still work as expected for Musl.
    
    Thus the morello-specific changes are not needed anymore now that brk
    is properly disabled in the kernel.
    
    Change-Id: Ida9b9341f22874ce113c5fda838c70980fe5394b

--- src/linux/sbrk.c
+++ src/linux/sbrk.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <errno.h>
-#ifdef __CHERI_PURE_CAPABILITY__
 #include "syscall.h"
 
 void *sbrk(intptr_t inc)
@@ -10,9 +9,3 @@ void *sbrk(intptr_t inc)
 	if (inc) return (void *)__syscall_ret(-ENOMEM);
 	return (void *)__syscall(SYS_brk, 0);
 }
-#else
-void *sbrk(intptr_t inc)
-{
-	__syscall_ret(-ENOMEM);
-}
-#endif

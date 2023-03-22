CHERI-Alliance/musl

commit 60029bbd149ab40059284bacbcb8931abab84f2c

Morello: Restrict mmap bounds and permissions
Restrict mmap allocations to appropriate bounds requested and set
appropriate permissions based on usage through libc.

Remove branch to sealed pair permision from root cap permissions.

Change-Id: Ibbb518be26416092e6a1bc293e0a1f5dedd28fa6

src/thread/sem_open.c
@@ -13,6 +13,7 @@
 #include <pthread.h>
 #include "lock.h"
 #include "fork_impl.h"
+#include "cheri_helpers.h"
 
 #define malloc __libc_malloc
 #define calloc __libc_calloc
@@ -87,11 +88,16 @@ sem_t *sem_open(const char *name, int flags, ...)
 		if (flags != (O_CREAT|O_EXCL)) {
 			fd = open(name, FLAGS);
 			if (fd >= 0) {
-				if (fstat(fd, &st) < 0 ||
-				    (map = mmap(0, sizeof(sem_t), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) {
+				map = mmap(0, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+				if (fstat(fd, &st) < 0 || map == MAP_FAILED) {
 					close(fd);
 					goto fail;
 				}
+#ifdef __CHERI_PURE_CAPABILITY__
+				/* TODO: Remove after mmap implementation. */
+				map = __builtin_cheri_bounds_set(map, sizeof(sem_t));
+				map = __builtin_cheri_perms_and(map, MUSL_CAP_PROT_SEM);
+#endif
 				close(fd);
 				break;
 			}
@@ -121,12 +127,19 @@ sem_t *sem_open(const char *name, int flags, ...)
 			if (errno == EEXIST) continue;
 			goto fail;
 		}
+
+		map = mmap(0, sizeof(sem_t), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 		if (write(fd, &newsem, sizeof newsem) != sizeof newsem || fstat(fd, &st) < 0 ||
-		    (map = mmap(0, sizeof(sem_t), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) {
+		    map == MAP_FAILED) {
 			close(fd);
 			unlink(tmp);
 			goto fail;
 		}
+#ifdef __CHERI_PURE_CAPABILITY__
+		/* TODO: Remove after mmap implementation. */
+		map = __builtin_cheri_bounds_set(map, sizeof(sem_t));
+		map = __builtin_cheri_perms_and(map, MUSL_CAP_PROT_SEM);
+#endif
 		close(fd);
 		e = link(tmp, name) ? errno : 0;
 		unlink(tmp);

src/include/cheri_helpers.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <cap_perms.h>
 
 #ifdef __CHERI_PURE_CAPABILITY__
 
@@ -24,6 +25,12 @@ inline void *restrict_bounds_to_tail(void *cap) {
   size_t len = __builtin_cheri_length_get(cap);
   return __builtin_cheri_bounds_set(cap, len - off);
 }
+
+#define MUSL_CAP_PROT_MAPFILE (READ_CAP_PERMS | __CHERI_CAP_PERMISSION_GLOBAL__)
+#define MUSL_CAP_PROT_THREAD (READ_CAP_PERMS | WRITE_CAP_PERMS | __CHERI_CAP_PERMISSION_GLOBAL__)
+#define MUSL_CAP_PROT_MALLOC (READ_CAP_PERMS | WRITE_CAP_PERMS | ROOT_CAP_PERMS)
+#define MUSL_CAP_PROT_SEM (READ_CAP_PERMS | WRITE_CAP_PERMS | __CHERI_CAP_PERMISSION_GLOBAL__)
+#define MUSL_CAP_PROT_NONE (0)
 
 #else
 

src/internal/cap_perms.h
@@ -8,16 +8,9 @@
 /**
  * ROOT_CAP_PERMS
  */
-#ifdef __ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__
-#define ROOT_CAP_PERMS \
-	__ARM_CAP_PERMISSION_BRANCH_SEALED_PAIR__ \
-	| __CHERI_CAP_PERMISSION_GLOBAL__ \
-	| __CHERI_CAP_PERMISSION_VMEM__
-#else
 #define ROOT_CAP_PERMS \
 	__CHERI_CAP_PERMISSION_GLOBAL__ \
 	| __CHERI_CAP_PERMISSION_VMEM__
-#endif
 
 /**
  * READ_CAP_PERMS

src/malloc/lite_malloc.c
@@ -7,6 +7,7 @@
 #include "lock.h"
 #include "syscall.h"
 #include "fork_impl.h"
+#include "cheri_helpers.h"
 
 #define ALIGN 16
 
@@ -88,6 +89,13 @@ static void *__simple_malloc(size_t n)
 			}
 			void *mem = __mmap(0, req, PROT_READ|PROT_WRITE,
 				MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+#ifdef __CHERI_PURE_CAPABILITY__
+			if (mem != MAP_FAILED) {
+				/* TODO: Remove after mmap implementation. */
+				mem = __builtin_cheri_bounds_set(mem, req);
+				mem = __builtin_cheri_perms_and(mem, MUSL_CAP_PROT_MALLOC);
+			}
+#endif
 			if (mem == MAP_FAILED || !new_area) {
 				UNLOCK(lock);
 				return mem==MAP_FAILED ? 0 : mem;

src/malloc/mallocng/malloc.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <errno.h>
+#include "cheri_helpers.h"
 
 #include "meta.h"
 
@@ -106,11 +107,19 @@ struct meta *alloc_meta(void)
 #endif
 		if (!ctx.avail_meta_area_count) {
 			size_t n = 2UL << ctx.meta_alloc_shift;
+#ifdef __CHERI_PURE_CAPABILITY__
+			p = mmap(0, n*pagesize, PROT_NONE | PROT_MAX(PROT_READ | PROT_WRITE),
+				MAP_PRIVATE|MAP_ANON, -1, 0);
+#else
 			p = mmap(0, n*pagesize, PROT_NONE,
 				MAP_PRIVATE|MAP_ANON, -1, 0);
+#endif
 			if (p==MAP_FAILED) return 0;
 #ifdef __CHERI_PURE_CAPABILITY__
 			p = restrict_perms(p);
+			/* TODO: Remove after mmap implementation. */
+			p = __builtin_cheri_bounds_set(p, n*pagesize);
+			p = __builtin_cheri_perms_and(p, MUSL_CAP_PROT_MALLOC);
 #endif
 			ctx.avail_meta_areas = p + pagesize;
 			ctx.avail_meta_area_count = (n-1)*(pagesize>>12);
@@ -282,7 +291,10 @@ static struct meta *alloc_group(int sc, size_t req)
 			return 0;
 		}
 #ifdef __CHERI_PURE_CAPABILITY__
-			p = restrict_perms(p);
+		p = restrict_perms(p);
+		/* TODO: Remove after mmap implementation. */
+		p = __builtin_cheri_bounds_set(p, needed);
+		p = __builtin_cheri_perms_and(p, MUSL_CAP_PROT_MALLOC);
 #endif
 		m->maplen = needed>>12;
 		ctx.mmap_counter++;
@@ -351,6 +363,9 @@ void *malloc_aligned(size_t n, size_t align)
 		if (p==MAP_FAILED) return 0;
 #ifdef __CHERI_PURE_CAPABILITY__
 		p = restrict_perms(p);
+		/* TODO: Remove after mmap implementation. */
+		p = __builtin_cheri_bounds_set(p, ((needed+4095)/4096) * 4096);
+		p = __builtin_cheri_perms_and(p, MUSL_CAP_PROT_MALLOC);
 #endif
 		wrlock();
 		step_seq();

src/malloc/mallocng/mallocmap.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <stddef.h>
+#include "cheri_helpers.h"
 
 #include "mallocmap.h"
 
@@ -17,7 +18,7 @@ Quadratic probing is used in case of hash collision
 Tab indices and hash are size_t
 
 Only the key's address is used for calculating the hash - the
- capaility is simply preserved for validation against the provided
+ capability is simply preserved for validation against the provided
  capability once the entry is found.
 
 Lazy deletion is used to avoid overly expensive deletions.
@@ -92,6 +93,11 @@ static int mallocmap_resize(size_t nel, struct __mallocmap_tab *htab)
 		htab->entries = oldtab;
 		return 0;
 	}
+
+#ifdef __CHERI_PURE_CAPABILITY__
+	htab->entries = __builtin_cheri_bounds_set(htab->entries, map_size);
+	htab->entries = __builtin_cheri_perms_and(htab->entries, MUSL_CAP_PROT_MALLOC);
+#endif
 
 	htab->mask = newsize - 1;
 

src/malloc/mallocng/meta.h
@@ -118,7 +118,8 @@ static inline void *expand_bounds(void *p) {
 }
 
 static inline void *restrict_user_ptr(void *p, size_t len) {
-	return __builtin_cheri_bounds_set(p, len);
+	return __builtin_cheri_perms_and(__builtin_cheri_bounds_set(p, len),
+		__CHERI_CAP_PERMISSION_GLOBAL__ | READ_CAP_PERMS | WRITE_CAP_PERMS);
 }
 
 static inline void *restrict_perms(void *p) {

src/malloc/oldmalloc/malloc.c
@@ -10,6 +10,7 @@
 #include "pthread_impl.h"
 #include "malloc_impl.h"
 #include "fork_impl.h"
+#include "cheri_helpers.h"
 
 #define malloc __libc_malloc_impl
 #define realloc __libc_realloc
@@ -188,6 +189,13 @@ static void *__expand_heap(size_t *pn)
 	void *area = __mmap(0, n, PROT_READ|PROT_WRITE,
 		MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 	if (area == MAP_FAILED) return 0;
+
+#ifdef __CHERI_PURE_CAPABILITY__
+	/* TODO: Remove after mmap implementation. */
+	area = __builtin_cheri_bounds_set(area, n);
+	area = __builtin_cheri_perms_and(area, MUSL_CAP_PROT_MALLOC);
+#endif
+
 	*pn = n;
 	mmap_step++;
 	return area;
@@ -302,7 +310,14 @@ void *malloc(size_t n)
 		size_t len = n + OVERHEAD + PAGE_SIZE - 1 & -PAGE_SIZE;
 		char *base = __mmap(0, len, PROT_READ|PROT_WRITE,
 			MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-		if (base == (void *)-1) return 0;
+		if (base == MAP_FAILED) return 0;
+
+#ifdef __CHERI_PURE_CAPABILITY__
+	/* TODO: Remove after mmap implementation. */
+	base = __builtin_cheri_bounds_set(base, len);
+	base = __builtin_cheri_perms_and(base, MUSL_CAP_PROT_MALLOC);
+#endif
+		
 		c = (void *)(base + SIZE_ALIGN - OVERHEAD);
 		c->csize = len - (SIZE_ALIGN - OVERHEAD);
 		c->psize = SIZE_ALIGN - OVERHEAD;

commit 3216cb79d3b198e30a13f4b5778a4e235ad58d31

sem_open: Fix possible memory leak
This change partially reverts 60029bbd149ab40059284bacbcb8931abab84f2c,
which introduced a bug: allocated memory might not be released upon
an error in some paths.

Co-authored-by: Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>
Change-Id: I257bdc498ef1e0ec16fe032b8454548faa2f1c8a

src/thread/sem_open.c
@@ -88,8 +88,8 @@ sem_t *sem_open(const char *name, int flags, ...)
 		if (flags != (O_CREAT|O_EXCL)) {
 			fd = open(name, FLAGS);
 			if (fd >= 0) {
-				map = mmap(0, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-				if (fstat(fd, &st) < 0 || map == MAP_FAILED) {
+				if (fstat(fd, &st) < 0 ||
+				    (map = mmap(0, sizeof(sem_t), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) {
 					close(fd);
 					goto fail;
 				}
@@ -128,9 +128,8 @@ sem_t *sem_open(const char *name, int flags, ...)
 			goto fail;
 		}
 
-		map = mmap(0, sizeof(sem_t), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 		if (write(fd, &newsem, sizeof newsem) != sizeof newsem || fstat(fd, &st) < 0 ||
-		    map == MAP_FAILED) {
+		    (map = mmap(0, sizeof(sem_t), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) {
 			close(fd);
 			unlink(tmp);
 			goto fail;
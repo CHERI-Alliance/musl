CHERI-Alliance/musl

commit d9b4140bec32499262a09396193288f1064c20f0

Morello: Switch to AT_CHERI_* cap in init_tls
Change-Id: I0bcf1ebea1640a054efb9a7fb4ed5e9bb0b31eab

src/env/__init_tls.c
@@ -81,16 +81,20 @@ extern weak hidden const size_t _DYNAMIC[];
 
 static void static_init_tls(uintptr_t *aux)
 {
-	unsigned char *p;
+	unsigned char *p, *aux_at_phdr;
 	size_t n;
 	Phdr *phdr, *tls_phdr=0;
 	uintptr_t base = 0;
 	void *mem;
-
-	for (p=(void *)aux[AT_PHDR],n=aux[AT_PHNUM]; n; n--,p+=aux[AT_PHENT]) {
+#if defined(__CHERI_PURE_CAPABILITY__) && defined(LIBSHIM)
+	aux_at_phdr = p = __builtin_cheri_address_set(aux[AT_CHERI_EXEC_RX_CAP], (size_t) aux[AT_PHDR]);
+#else
+	aux_at_phdr = p = (void *)aux[AT_PHDR];
+#endif
+	for (n=aux[AT_PHNUM]; n; n--,p+=aux[AT_PHENT]) {
 		phdr = (void *)p;
 		if (phdr->p_type == PT_PHDR)
-			base = aux[AT_PHDR] - phdr->p_vaddr;
+			base = aux_at_phdr - phdr->p_vaddr;
 		if (phdr->p_type == PT_DYNAMIC && _DYNAMIC)
 			base = (size_t)_DYNAMIC - phdr->p_vaddr;
 		if (phdr->p_type == PT_TLS)

src/ldso/dl_iterate_phdr.c
@@ -6,21 +6,32 @@ extern weak hidden const size_t _DYNAMIC[];
 
 static int static_dl_iterate_phdr(int(*callback)(struct dl_phdr_info *info, size_t size, void *data), void *data)
 {
-	unsigned char *p;
+	unsigned char *p, *aux_at_phdr;
+	void * rx_cap;
 	ElfW(Phdr) *phdr, *tls_phdr=0;
 	size_t base = 0;
 	size_t n;
 	struct dl_phdr_info info;
 	size_t i, aux[AUX_CNT] = {0};
 
-	for (i=0; libc.auxv[i].a_type; i++)
+	for (i=0; libc.auxv[i].a_type; i++) {
 		if (libc.auxv[i].a_type<AUX_CNT)
 			aux[libc.auxv[i].a_type] = libc.auxv[i].a_un.a_val;
+#ifdef __CHERI_PURE_CAPABLIITY__
+		if (libc.auxv[i].a_type==AT_CHERI_EXEC_RX_CAP)
+			rx_cap = libc.auxv[i].a_un.a_ptr;
+#endif
+	}
 
+#if defined(__CHERI_PURE_CAPABILITY__) && defined(LIBSHIM)
+	aux_at_phdr = p = __builtin_cheri_address_set(rx_cap, (size_t) aux[AT_PHDR]);
+#else
+	aux_at_phdr = p = (void *)aux[AT_PHDR];
+#endif
 	for (p=(void *)aux[AT_PHDR],n=aux[AT_PHNUM]; n; n--,p+=aux[AT_PHENT]) {
 		phdr = (void *)p;
 		if (phdr->p_type == PT_PHDR)
-			base = aux[AT_PHDR] - phdr->p_vaddr;
+			base = aux_at_phdr - phdr->p_vaddr;
 		if (phdr->p_type == PT_DYNAMIC && _DYNAMIC)
 			base = (size_t)_DYNAMIC - phdr->p_vaddr;
 		if (phdr->p_type == PT_TLS)

commit c653e6e888277996e1add9bab1db3cbb2e94223d

tls: add proper cast in static_init_tls
Commit d9b4140bec32 ("Morello: Switch to AT_CHERI_* cap in init_tls")
changed the variable in an expression from a data to a pointer type,
which makes the result a pointer as well.

Add a cast to change that back into a data type, so that compilers like
GCC do not warn about the implicit conversion.

Fixes: d9b4140bec32 ("Morello: Switch to AT_CHERI_* cap in init_tls")
Change-Id: Ia8dc1dedd9c978770aec4229fc515640c8437abd

src/env/__init_tls.c
@@ -97,7 +97,7 @@ static void static_init_tls(uintptr_t *aux)
 	for (n=aux[AT_PHNUM]; n; n--,p+=aux[AT_PHENT]) {
 		phdr = (void *)p;
 		if (phdr->p_type == PT_PHDR)
-			base = aux_at_phdr - phdr->p_vaddr;
+			base = (ptraddr_t)(aux_at_phdr - phdr->p_vaddr);
 		if (phdr->p_type == PT_DYNAMIC && _DYNAMIC)
 			base = (size_t)_DYNAMIC - phdr->p_vaddr;
 		if (phdr->p_type == PT_TLS)
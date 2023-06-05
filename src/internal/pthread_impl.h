CHERI-Alliance/musl

commit 9ad5f0fd84ec272ef126d84daf1ac1fac0741f6a

New option to choose compiler builtins
A new configuration option '--enable-builtins' controls whether
musl-libc will use hand-rolled assembly snippets or rather use
compiler builtins. This option is disabled by default, because
it is not compatible with older compilers and all targets.

This change mostly focuses on 'atomics.h' and introduces some
separation:
  - atomic.h is the main internal header for atomics
  - atomic_fallbacks.h contains the fallback implementations
  - atomic_builtins.h uses compiler builtins to implement
    most atomics

In addition, __get_tp() can use __builtin_thread_pointer().

This change enables this new feature for aarch64, morello and
x86_64, to start with. These architectures were checked for
correctness with an old enough GCC/Clang.

Change-Id: I99d3548d5ce283b25f2c08f155a1fb0cc6bd1f09

src/internal/pthread_impl.h
@@ -13,6 +13,13 @@
 
 #include "pthread_arch.h"
 
+#if defined(MUSL_USE_COMPILER_BUILTINS)
+static inline uintptr_t __get_tp()
+{
+	return __builtin_thread_pointer();
+}
+#endif
+
 #define pthread __pthread
 
 struct pthread {

arch/aarch64/atomic_arch.h
@@ -1,3 +1,5 @@
+#if !defined(MUSL_USE_COMPILER_BUILTINS)
+
 #define a_ll a_ll
 static inline int a_ll(volatile int *p)
 {
@@ -80,3 +82,5 @@ static inline int a_clz_64(uint64_t x)
 	__asm__("clz %0, %1" : "=r"(x) : "r"(x));
 	return x;
 }
+
+#endif  // !defined(MUSL_USE_COMPILER_BUILTINS)

arch/aarch64/pthread_arch.h
@@ -1,9 +1,11 @@
+#if !defined(MUSL_USE_COMPILER_BUILTINS)
 static inline uintptr_t __get_tp()
 {
 	uintptr_t tp;
 	__asm__ ("mrs %0,tpidr_el0" : "=r"(tp));
 	return tp;
 }
+#endif
 
 #define TLS_ABOVE_TP
 #define GAP_ABOVE_TP 16

arch/morello/atomic_arch.h
@@ -1,3 +1,5 @@
+#if !defined(MUSL_USE_COMPILER_BUILTINS)
+
 #define a_ll a_ll
 static inline int a_ll(volatile int *p)
 {
@@ -80,3 +82,5 @@ static inline int a_clz_64(uint64_t x)
 	__asm__("clz %0, %1" : "=r"(x) : "r"(x));
 	return x;
 }
+
+#endif  // !defined(MUSL_USE_COMPILER_BUILTINS)

arch/x86_64/atomic_arch.h
@@ -1,3 +1,5 @@
+#if !defined(MUSL_USE_COMPILER_BUILTINS)
+
 #define a_cas a_cas
 static inline int a_cas(volatile int *p, int t, int s)
 {
@@ -90,24 +92,6 @@ static inline void a_store(volatile int *p, int x)
 		: "=m"(*p) : "r"(x) : "memory" );
 }
 
-#define a_barrier a_barrier
-static inline void a_barrier()
-{
-	__asm__ __volatile__( "" : : : "memory" );
-}
-
-#define a_spin a_spin
-static inline void a_spin()
-{
-	__asm__ __volatile__( "pause" : : : "memory" );
-}
-
-#define a_crash a_crash
-static inline void a_crash()
-{
-	__asm__ __volatile__( "hlt" : : : "memory" );
-}
-
 #define a_ctz_64 a_ctz_64
 static inline int a_ctz_64(uint64_t x)
 {
@@ -120,4 +104,26 @@ static inline int a_clz_64(uint64_t x)
 {
 	__asm__( "bsr %1,%0 ; xor $63,%0" : "=r"(x) : "r"(x) );
 	return x;
+}
+
+#define a_barrier a_barrier
+static inline void a_barrier()
+{
+	__asm__ __volatile__( "" : : : "memory" );
+}
+
+#endif  // !defined(MUSL_USE_COMPILER_BUILTINS)
+
+// The functions below have no compiler-builtin equivalents.
+
+#define a_spin a_spin
+static inline void a_spin()
+{
+	__asm__ __volatile__( "pause" : : : "memory" );
+}
+
+#define a_crash a_crash
+static inline void a_crash()
+{
+	__asm__ __volatile__( "hlt" : : : "memory" );
 }

arch/x86_64/pthread_arch.h
@@ -1,8 +1,10 @@
+#if !defined(MUSL_USE_COMPILER_BUILTINS)
 static inline uintptr_t __get_tp()
 {
 	uintptr_t tp;
 	__asm__ ("mov %%fs:0,%0" : "=r" (tp) );
 	return tp;
 }
+#endif
 
 #define MC_PC gregs[REG_RIP]

configure
@@ -35,6 +35,7 @@ Optional features:
   --enable-wrapper=...    build given musl toolchain wrapper [auto]
   --enable-shared         enable building shared library [disabled]
   --disable-static        inhibit building static library [enabled]
+  --enable-builtins       prefer compiler builtins when possible [disabled]
   --disable-morello       inhibit using Morello features (only for AArch64 targets) [enabled]
   --disable-libshim       inhibit building with syscall libshim [auto]
   --libshim-path=<path>   path to libshim [libshim]
@@ -168,6 +169,7 @@ wrapper=auto
 gcc_wrapper=no
 clang_wrapper=no
 malloc_dir=mallocng
+enable_builtins=no
 
 for arg ; do
 case "$arg" in
@@ -204,6 +206,8 @@ case "$arg" in
 --enable-gcc-wrapper|--enable-gcc-wrapper=yes) wrapper=yes ; gcc_wrapper=yes ;;
 --disable-gcc-wrapper|--enable-gcc-wrapper=no) wrapper=no ;;
 --with-malloc=*) malloc_dir=${arg#*=} ;;
+--enable-builtins|--enable-builtins=yes) enable_builtins=yes ;;
+--disable-builtins|--enable-builtins=no) enable_builtins=no ;;
 --enable-*|--disable-*|--with-*|--without-*|--*dir=*) ;;
 --host=*|--target=*) target=${arg#*=} ;;
 --build=*) build=${arg#*=} ;;
@@ -527,6 +531,18 @@ if test "$libshim" = yes ; then
     else
         printf "%s (does not exist)\n" $LIBARCHCAP_PATH
     fi
+fi
+
+#
+# Use compiler builtins as much as possible, if requested.
+# This switch is off by default for backward compatibility.
+#
+if test "$enable_builtins" = yes ; then
+  case "$ARCH" in
+    aarch64|morello|x86_64) ;;
+    *) fail "$0: $ARCH is not checked for compiler builtins yet";;
+  esac
+  CFLAGS_AUTO="$CFLAGS_AUTO -DMUSL_USE_COMPILER_BUILTINS"
 fi
 
 #

src/internal/atomic.h
@@ -4,330 +4,7 @@
 #include <stdint.h>
 
 #include "atomic_arch.h"
+#include "atomic_builtins.h"
+#include "atomic_fallbacks.h"
 
-#ifdef a_ll
-
-#ifndef a_pre_llsc
-#define a_pre_llsc()
-#endif
-
-#ifndef a_post_llsc
-#define a_post_llsc()
-#endif
-
-#ifndef a_cas
-#define a_cas a_cas
-static inline int a_cas(volatile int *p, int t, int s)
-{
-	int old;
-	a_pre_llsc();
-	do old = a_ll(p);
-	while (old==t && !a_sc(p, s));
-	a_post_llsc();
-	return old;
-}
-#endif
-
-#ifndef a_swap
-#define a_swap a_swap
-static inline int a_swap(volatile int *p, int v)
-{
-	int old;
-	a_pre_llsc();
-	do old = a_ll(p);
-	while (!a_sc(p, v));
-	a_post_llsc();
-	return old;
-}
-#endif
-
-#ifndef a_fetch_add
-#define a_fetch_add a_fetch_add
-static inline int a_fetch_add(volatile int *p, int v)
-{
-	int old;
-	a_pre_llsc();
-	do old = a_ll(p);
-	while (!a_sc(p, (unsigned)old + v));
-	a_post_llsc();
-	return old;
-}
-#endif
-
-#ifndef a_fetch_and
-#define a_fetch_and a_fetch_and
-static inline int a_fetch_and(volatile int *p, int v)
-{
-	int old;
-	a_pre_llsc();
-	do old = a_ll(p);
-	while (!a_sc(p, old & v));
-	a_post_llsc();
-	return old;
-}
-#endif
-
-#ifndef a_fetch_or
-#define a_fetch_or a_fetch_or
-static inline int a_fetch_or(volatile int *p, int v)
-{
-	int old;
-	a_pre_llsc();
-	do old = a_ll(p);
-	while (!a_sc(p, old | v));
-	a_post_llsc();
-	return old;
-}
-#endif
-
-#endif
-
-#ifdef a_ll_p
-
-#ifndef a_cas_p
-#define a_cas_p a_cas_p
-static inline void *a_cas_p(volatile void *p, void *t, void *s)
-{
-	void *old;
-	a_pre_llsc();
-	do old = a_ll_p(p);
-	while (old==t && !a_sc_p(p, s));
-	a_post_llsc();
-	return old;
-}
-#endif
-
-#endif
-
-#ifndef a_cas
-#error missing definition of a_cas
-#endif
-
-#ifndef a_swap
-#define a_swap a_swap
-static inline int a_swap(volatile int *p, int v)
-{
-	int old;
-	do old = *p;
-	while (a_cas(p, old, v) != old);
-	return old;
-}
-#endif
-
-#ifndef a_fetch_add
-#define a_fetch_add a_fetch_add
-static inline int a_fetch_add(volatile int *p, int v)
-{
-	int old;
-	do old = *p;
-	while (a_cas(p, old, (unsigned)old+v) != old);
-	return old;
-}
-#endif
-
-#ifndef a_fetch_and
-#define a_fetch_and a_fetch_and
-static inline int a_fetch_and(volatile int *p, int v)
-{
-	int old;
-	do old = *p;
-	while (a_cas(p, old, old&v) != old);
-	return old;
-}
-#endif
-#ifndef a_fetch_or
-#define a_fetch_or a_fetch_or
-static inline int a_fetch_or(volatile int *p, int v)
-{
-	int old;
-	do old = *p;
-	while (a_cas(p, old, old|v) != old);
-	return old;
-}
-#endif
-
-#ifndef a_and
-#define a_and a_and
-static inline void a_and(volatile int *p, int v)
-{
-	a_fetch_and(p, v);
-}
-#endif
-
-#ifndef a_or
-#define a_or a_or
-static inline void a_or(volatile int *p, int v)
-{
-	a_fetch_or(p, v);
-}
-#endif
-
-#ifndef a_inc
-#define a_inc a_inc
-static inline void a_inc(volatile int *p)
-{
-	a_fetch_add(p, 1);
-}
-#endif
-
-#ifndef a_dec
-#define a_dec a_dec
-static inline void a_dec(volatile int *p)
-{
-	a_fetch_add(p, -1);
-}
-#endif
-
-#ifndef a_store
-#define a_store a_store
-static inline void a_store(volatile int *p, int v)
-{
-#ifdef a_barrier
-	a_barrier();
-	*p = v;
-	a_barrier();
-#else
-	a_swap(p, v);
-#endif
-}
-#endif
-
-#ifndef a_barrier
-#define a_barrier a_barrier
-static void a_barrier()
-{
-	volatile int tmp = 0;
-	a_cas(&tmp, 0, 0);
-}
-#endif
-
-#ifndef a_spin
-#define a_spin a_barrier
-#endif
-
-#ifndef a_and_64
-#define a_and_64 a_and_64
-static inline void a_and_64(volatile uint64_t *p, uint64_t v)
-{
-	union { uint64_t v; uint32_t r[2]; } u = { v };
-	if (u.r[0]+1) a_and((int *)p, u.r[0]);
-	if (u.r[1]+1) a_and((int *)p+1, u.r[1]);
-}
-#endif
-
-#ifndef a_or_64
-#define a_or_64 a_or_64
-static inline void a_or_64(volatile uint64_t *p, uint64_t v)
-{
-	union { uint64_t v; uint32_t r[2]; } u = { v };
-	if (u.r[0]) a_or((int *)p, u.r[0]);
-	if (u.r[1]) a_or((int *)p+1, u.r[1]);
-}
-#endif
-
-#ifndef a_cas_p
-typedef char a_cas_p_undefined_but_pointer_not_32bit[-sizeof(char) == 0xffffffff ? 1 : -1];
-#define a_cas_p a_cas_p
-static inline void *a_cas_p(volatile void *p, void *t, void *s)
-{
-	return (void *)a_cas((volatile int *)p, (int)t, (int)s);
-}
-#endif
-
-#ifndef a_or_l
-#define a_or_l a_or_l
-static inline void a_or_l(volatile void *p, long v)
-{
-	if (sizeof(long) == sizeof(int)) a_or(p, v);
-	else a_or_64(p, v);
-}
-#endif
-
-#ifndef a_crash
-#define a_crash a_crash
-static inline void a_crash()
-{
-	*(volatile char *)0=0;
-}
-#endif
-
-#ifndef a_ctz_32
-#define a_ctz_32 a_ctz_32
-static inline int a_ctz_32(uint32_t x)
-{
-#ifdef a_clz_32
-	return 31-a_clz_32(x&-x);
-#else
-	static const char debruijn32[32] = {
-		0, 1, 23, 2, 29, 24, 19, 3, 30, 27, 25, 11, 20, 8, 4, 13,
-		31, 22, 28, 18, 26, 10, 7, 12, 21, 17, 9, 6, 16, 5, 15, 14
-	};
-	return debruijn32[(x&-x)*0x076be629 >> 27];
-#endif
-}
-#endif
-
-#ifndef a_ctz_64
-#define a_ctz_64 a_ctz_64
-static inline int a_ctz_64(uint64_t x)
-{
-	static const char debruijn64[64] = {
-		0, 1, 2, 53, 3, 7, 54, 27, 4, 38, 41, 8, 34, 55, 48, 28,
-		62, 5, 39, 46, 44, 42, 22, 9, 24, 35, 59, 56, 49, 18, 29, 11,
-		63, 52, 6, 26, 37, 40, 33, 47, 61, 45, 43, 21, 23, 58, 17, 10,
-		51, 25, 36, 32, 60, 20, 57, 16, 50, 31, 19, 15, 30, 14, 13, 12
-	};
-	if (sizeof(long) < 8) {
-		uint32_t y = x;
-		if (!y) {
-			y = x>>32;
-			return 32 + a_ctz_32(y);
-		}
-		return a_ctz_32(y);
-	}
-	return debruijn64[(x&-x)*0x022fdd63cc95386dull >> 58];
-}
-#endif
-
-static inline int a_ctz_l(unsigned long x)
-{
-	return (sizeof(long) < 8) ? a_ctz_32(x) : a_ctz_64(x);
-}
-
-#ifndef a_clz_64
-#define a_clz_64 a_clz_64
-static inline int a_clz_64(uint64_t x)
-{
-#ifdef a_clz_32
-	if (x>>32)
-		return a_clz_32(x>>32);
-	return a_clz_32(x) + 32;
-#else
-	uint32_t y;
-	int r;
-	if (x>>32) y=x>>32, r=0; else y=x, r=32;
-	if (y>>16) y>>=16; else r |= 16;
-	if (y>>8) y>>=8; else r |= 8;
-	if (y>>4) y>>=4; else r |= 4;
-	if (y>>2) y>>=2; else r |= 2;
-	return r | !(y>>1);
-#endif
-}
-#endif
-
-#ifndef a_clz_32
-#define a_clz_32 a_clz_32
-static inline int a_clz_32(uint32_t x)
-{
-	x >>= 1;
-	x |= x >> 1;
-	x |= x >> 2;
-	x |= x >> 4;
-	x |= x >> 8;
-	x |= x >> 16;
-	x++;
-	return 31-a_ctz_32(x);
-}
-#endif
-
-#endif
+#endif  // _ATOMIC_H

commit bc19702e0500108bf71cb3ebb7eaa7f4a7511db2

builtins: Add missing cast for __builtin_thread_pointer()
With --enable-builtins, we use __builtin_thread_pointer() for the
implementation of __get_tp(). However the former returns a void*,
whereas the return type for the latter is a uintptr_t.

Add an explicit cast to make the types match.
This fixes compilation with GCC.

Fixes: 9ad5f0fd84ec ("New option to choose compiler builtins")
Change-Id: Ia8dc1dedd9c978770aec4229fc515640c8437abd

src/internal/pthread_impl.h
@@ -16,7 +16,7 @@
 #if defined(MUSL_USE_COMPILER_BUILTINS)
 static inline uintptr_t __get_tp()
 {
-	return __builtin_thread_pointer();
+	return (uintptr_t)__builtin_thread_pointer();
 }
 #endif
 
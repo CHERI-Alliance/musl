CHERI-Alliance/musl

commit 4d06a81c9fd302e4aea28088c0d9fba487eff506

Morello: Switch to argc/argv/envp/auxv supplied in registers
This is a temporary solution to consume argc/argv/envp/auxv
supplied in C0, C1, C2, and C3 registers. Additional work needs
to be done to fully switch from using stack as source for these
items and only rely in values in register.

Co-authored-by: Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>

Change-Id: Id93a1d4a0f193fe94b9b41ddb1af1b9b2da9cd12

src/env/__libc_start_main.c
@@ -18,15 +18,21 @@ weak_alias(dummy1, __init_ssp);
 #ifdef __GNUC__
 __attribute__((__noinline__))
 #endif
-void __init_libc(char **envp, char *pn)
+void __init_libc(char **envp, auxv_entry *auxv, char *pn)
 {
 	size_t i = 0;
-	uintptr_t *auxv, aux[AUX_CNT] = { 0 };
+	uintptr_t aux[AUX_CNT] = { 0 };
 	__environ = envp;
-	for (i=0; envp[i]; i++);
-	auxv = (void *)(envp+i+1);
-	libc.auxv = (auxv_entry *) auxv;
-	for (i=0; auxv[i]; i+=2) if (auxv[i]<AUX_CNT) aux[auxv[i]] = auxv[i+1];
+	libc.auxv = auxv;
+	for (i=0; auxv[i].a_type != AT_NULL; i++) {
+		if (auxv[i].a_type<AUX_CNT) {
+#ifdef __CHERI_PURE_CAPABILITY__
+			aux[auxv[i].a_type] = auxv[i].a_un.a_ptr;
+#else
+			aux[auxv[i].a_type] = auxv[i].a_un.a_val;
+#endif
+		}
+	}
 	__hwcap = aux[AT_HWCAP];
 	if (aux[AT_SYSINFO]) __sysinfo = aux[AT_SYSINFO];
 	libc.page_size = aux[AT_PAGESZ];
@@ -77,17 +83,17 @@ static void libc_start_init(void)
 
 weak_alias(libc_start_init, __libc_start_init);
 
-typedef int lsm2_fn(int (*)(int,char **,char **), int, char **);
+typedef int lsm2_fn(int (*)(int,char **,char **), int, char **, char **);
 static lsm2_fn libc_start_main_stage2;
 
-int __libc_start_main(int (*main)(int,char **,char **), int argc, char **argv)
+int __libc_start_main(int (*main)(int,char **,char **, char**),
+						int argc, char **argv, char **envp,
+						auxv_entry *auxv)
 {
-	char **envp = argv+argc+1;
-
 	/* External linkage, and explicit noinline attribute if available,
 	 * are used to prevent the stack frame used during init from
 	 * persisting for the entire process lifetime. */
-	__init_libc(envp, argv[0]);
+	__init_libc(envp, auxv, argv[0]);
 
 	/* Barrier against hoisting application code or anything using ssp
 	 * or thread pointer prior to its initialization above. */
@@ -99,7 +105,7 @@ int __libc_start_main(int (*main)(int,char **,char **), int argc, char **argv)
 #else
 	a_barrier();
 #endif
-	return stage2(main, argc, argv);
+	return stage2(main, argc, argv, envp);
 }
 
 #ifdef __CHERI_PURE_CAPABILITY__
@@ -110,9 +116,9 @@ static void cheri_set_bounds_on_cap_array(void ***array) {
 }
 #endif
 
-static int libc_start_main_stage2(int (*main)(int,char **,char **), int argc, char **argv)
+
+static int libc_start_main_stage2(int (*main)(int,char **,char **), int argc, char **argv, char **envp)
 {
-	char **envp = argv+argc+1;
 	__libc_start_init();
 
 #ifdef __CHERI_PURE_CAPABILITY__

Makefile
@@ -185,7 +185,7 @@ $(LIBSHIM_LIB): $(LIBSHIM_LIBC_PATH) $(LIBSHIM_PATH) FORCE
 	$(MAKE) -C $(LIBSHIM_PATH) LIBC=musl ARCH=$(ARCH) LIBC_PATH=$(LIBSHIM_LIBC_PATH) \
 	CC=$(CC) CXX=$(CC)++ AR=$(AR) RANLIB=$(RANLIB) CFLAGS="$(LIBSHIM_FLAGS)" CXXFLAGS="$(LIBSHIM_FLAGS)" \
 	LIBSHIM_JSON_PATH=$(shell realpath $(LIBSHIM_JSON_PATH)) \
-	DEFINES="-DLIBSHIM_LIBRARY_BUILD=1 -DLIBSHIM_ZERO_DDC=1 -DLIBSHIM_CANCELLATION_POINTS=1"
+	DEFINES="-DLIBSHIM_LIBRARY_BUILD=1 -DLIBSHIM_ZERO_DDC=1 -DLIBSHIM_CANCELLATION_POINTS=1 -DLIBSHIM_TRANSITION_SWITCH_ARGC_ARGV_ENVP_AUXV_QUADRUPLE=1"
 
 # install libc headers for libshim build
 $(LIBSHIM_LIBC_PATH)/include/bits/%: $(srcdir)/arch/$(ARCH)/bits/%

arch/morello/crt_arch.h
@@ -9,13 +9,14 @@ START ":\n"
 "	chktgd csp\n"
 "	b.cs .L1\n"
 "	mov x0, sp\n"
-"	cvtd c0, x0\n"
-"	bl do_raw_args_marshalling\n"
-"	mov csp, c0\n"
-"	b .L2\n"
+"	bl __shim_marshal_program_arguments\n"
+// Not needed, but shown here for reference
+// "	ldr c0, [csp, #0]\n" // ARGC
+// "	ldr c1, [csp, #16]\n" // ARGV
+// "	ldr c2, [csp, #32]\n" // ENVP
+// "	ldr c3, [csp, #48]\n" // AUXV
 ".L1:\n"
-"	mov c0, csp\n"
-".L2:\n"
+"	ldr c0, [csp, #48]\n"
 #else
 "	mov c0, csp\n"
 #endif
@@ -34,12 +35,14 @@ __asm__ (".text \n"
 ".global __morello_init_static\n"
 ".type __morello_init_static,%function\n"
 "__morello_init_static:\n"
+#ifndef LIBSHIM
 "	ldr     x10, [c0], #32\n" //Increment argc/NULL
 "	add     c0, c0, x10, lsl #4\n"
 ".Lloop_get_auxv:\n"
 "	ldr     c9, [c0], #16\n"
 "	cmp     c9, czr\n"
 "	b.ne    .Lloop_get_auxv\n"
+#endif
 ".Lloop_auxv:\n"
 "	ldp     x9, xzr, [c0], #16\n"
 "	cmp     x9, xzr\n"

crt/crt1.c
@@ -1,21 +1,26 @@
+#include <sys/auxv.h>
 #include <features.h>
 #include "libc.h"
+#include "cap_aux.h"
 
 #define START "_start"
 
 #include "crt_arch.h"
 
 int main();
-weak void _init();
-weak void _fini();
-int __libc_start_main(int (*)(), int, char **,
-	void (*)(), void(*)(), void(*)());
+int __libc_start_main(int (*)(), int, char **, char **, auxv_entry *);
 
 void _start_c(intptr_t *p)
 {
 	int argc = p[0];
 
 	char **argv = (void *)(p+1);
-
-	__libc_start_main(main, argc, argv, _init, _fini, 0);
+	IF_CHERI_GET_ARGV(p, argv);
+	char **envp = argv+argc+1;
+	IF_CHERI_GET_ENVP(p, envp);
+	int envp_i;
+	for (envp_i=0; envp[envp_i]; envp_i++);
+	auxv_entry *auxv = (void *)(envp+envp_i+1);
+	IF_CHERI_GET_AUXV(p, auxv);
+	__libc_start_main(main, argc, argv, envp, auxv);
 }

ldso/dlstart.c
@@ -41,12 +41,17 @@ hidden void _dlstart_c(uintptr_t *sp, size_t *dynv_raw)
 	Rel_t *rel_ptr;
 	Rela_t *rela_ptr;
 	char *base_rx, *base_rw;
+	auxv_entry *auxv;
 
 	int argc = *sp;
 	char **argv = (void *)(sp+1);
+	IF_CHERI_GET_ARGV(sp, argv);
 
-	for (i=argc+1; argv[i]; i++);
-	auxv_entry *auxv = (void *)(argv+i+1);
+	char **envp = argv+argc+1;
+	IF_CHERI_GET_ENVP(sp, envp);
+	for (i=0; envp[i]; i++);
+	auxv = (void *)(envp+i+1);
+	IF_CHERI_GET_AUXV(sp,auxv);
 	dynv_entry *dynv = (void *)dynv_raw;
 
 	for (i=0; i<AUX_CNT; i++) aux[i] = &aux_null;

ldso/dynlink.c
@@ -1745,8 +1745,12 @@ hidden void __dls2(unsigned char *base, unsigned char *base_rw, uintptr_t *sp)
 {
 	uintptr_t *auxv;
 	size_t argc = *sp;
+#if defined(__CHERI_PURE_CAPABILITY__) && defined(LIBSHIM)
+	IF_CHERI_GET_AUXV(sp, auxv);
+#else
 	for (auxv=sp+1+argc+1; *auxv; auxv++);
 	auxv++;
+#endif
 	if (DL_FDPIC) {
 		void *p1 = (void *)sp[-2];
 		void *p2 = (void *)sp[-1];
@@ -1844,8 +1848,10 @@ void __dls3(uintptr_t *sp, size_t *auxv)
 	char *phdr_rw;
 	int argc = *sp;
 	char **argv = (void *)(sp+1);
+	IF_CHERI_GET_ARGV(sp, argv);
 	char **argv_orig = argv;
 	char **envp = argv+argc+1;
+	IF_CHERI_GET_ENVP(sp, envp);
 
 	/* Find aux vector just past environ[] and use it to initialize
 	 * global data that may be needed before we can make syscalls. */
@@ -2122,7 +2128,7 @@ void __dls3(uintptr_t *sp, size_t *auxv)
 
 	errno = 0;
 
-	CRTJMP(AUX_PTR(aux[AT_ENTRY]), argv-1);
+	CRTJMP(AUX_PTR(aux[AT_ENTRY]), sp);
 	for(;;);
 }
 

src/internal/cap_aux.h
@@ -1,0 +1,19 @@
+#ifndef CHERI_HELPERS_H
+#define CHERI_HELPERS_H
+
+#if defined(__CHERI_PURE_CAPABILITY__) && defined(LIBSHIM)
+#define IF_CHERI_LOAD_PTR(v,s,n) v = *(s + n);
+#else
+#define IF_CHERI_LOAD_PTR(v,s,n) /* no-op */
+#endif
+
+/* Retrieve int argc from stack */
+#define IF_CHERI_GET_ARGC(sp, var) IF_CHERI_LOAD_PTR(var, sp, 0)
+/* Retrieve char **argv from stack */
+#define IF_CHERI_GET_ARGV(sp, var) IF_CHERI_LOAD_PTR(var, sp, 1)
+/* Retrieve char **envp from stack */
+#define IF_CHERI_GET_ENVP(sp, var) IF_CHERI_LOAD_PTR(var, sp, 2)
+/* Retrieve char *auxv from stack */
+#define IF_CHERI_GET_AUXV(sp, var) IF_CHERI_LOAD_PTR(var, sp, 3)
+
+#endif

src/internal/dynlink.h
@@ -11,6 +11,8 @@
 #include "cap_perms.h"
 #endif
 
+#include "cap_aux.h"
+
 #if UINTPTR_MAX == 0xffffffff
 typedef Elf32_Ehdr Ehdr;
 typedef Elf32_Phdr Phdr;

commit 8016eaf3843a334a71fafb48f195cb1b51b52144

init_libc: cast pointer to data type
The a_ptr member in the AUX vector is a pointer, but the array we copy
those values into in __init_libc(), is made of uintptr_t's.
GCC complains about this conversion from pointer to integer type without
a cast, so add one to make it happy.

Fixes: 4d06a81c9fd3 ("Morello: Switch to argc/argv/envp/auxv supplied in registers")
Change-Id: I5cc793bf476d111b6af1e3a59f0601b3361fa3c2

src/env/__libc_start_main.c
@@ -28,7 +28,7 @@ void __init_libc(char **envp, auxv_entry *auxv, char *pn)
 	libc.auxv = auxv;
 	for (i=0; auxv[i].a_type != AT_NULL; i++) {
 		if (auxv[i].a_type<AUX_CNT)
-			aux[auxv[i].a_type] = auxv[i].a_un.a_ptr;
+			aux[auxv[i].a_type] = (uintptr_t)auxv[i].a_un.a_ptr;
 	}
 	__hwcap = aux[AT_HWCAP];
 	if (aux[AT_SYSINFO]) __sysinfo = aux[AT_SYSINFO];
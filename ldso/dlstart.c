CHERI-Alliance/musl

commit e0d77a0b0881540296443981039dd83b48d79786

Morello: Some refactoring to dlstart.c
Depending on whether the dynamic linker is invoked
as an interpreter or as a program, the values of
'rw_cap' and 'rx_cap' change.

Change-Id: If4c001c07bc255fad3e0a31290fcb08ab1a29233

ldso/dlstart.c
@@ -44,7 +44,7 @@ hidden void _dlstart_c(uintptr_t *sp, size_t *dynv_raw)
 	size_t *rel, rel_size, rel_count;
 	Rel_t *rel_ptr;
 	Rela_t *rela_ptr;
-	char *base_rx, *rw_cap;
+	char *base_rx;
 #if !defined(__CHERI_PURE_CAPABILITY__)
 	auxv_entry *auxv;
 	int argc = *sp;
@@ -58,6 +58,16 @@ hidden void _dlstart_c(uintptr_t *sp, size_t *dynv_raw)
 	for (i=0; i<AUX_CNT; i++) aux[i] = &aux_null;
 	for (i=0; auxv[i].a_type; i++) if (auxv[i].a_type<AUX_CNT)
 		aux[auxv[i].a_type] = auxv + i;
+
+#ifdef __CHERI_PURE_CAPABILITY__
+	const _Bool is_interpreter = (AUX_PTR(aux[AT_CHERI_INTERP_RW_CAP]) != NULL);
+	char *const rw_cap = AUX_PTR(aux[is_interpreter ?
+	                                 AT_CHERI_INTERP_RW_CAP :
+	                                 AT_CHERI_EXEC_RW_CAP]);
+	char *const rx_cap = AUX_PTR(aux[is_interpreter ?
+	                                 AT_CHERI_INTERP_RX_CAP :
+	                                 AT_CHERI_EXEC_RX_CAP]);
+#endif
 
 #if DL_FDPIC
 	struct fdpic_loadseg *segs, fakeseg;
@@ -140,12 +150,11 @@ hidden void _dlstart_c(uintptr_t *sp, size_t *dynv_raw)
 	 * the load address as the difference between &_DYNAMIC and the
 	 * virtual address in the PT_DYNAMIC program header. */
 
-#ifndef __CHERI_PURE_CAPABILITY__
+	// AT_BASE is always a raw 64-bit value.
 	base_rx = AUX_PTR(aux[AT_BASE]);
-#else
-	base_rx = AUX_PTR(aux[AT_CHERI_INTERP_RX_CAP]);
-	rw_cap = AUX_PTR(aux[AT_CHERI_INTERP_RW_CAP]);
-	base_rx = __builtin_cheri_address_set(base_rx, AUX_VAL(aux[AT_BASE]));
+#ifdef __CHERI_PURE_CAPABILITY__
+	if (is_interpreter)
+		base_rx = __builtin_cheri_address_set(rx_cap, base_rx);
 #endif
 
 	if (!base_rx) {

commit 43c7d1037bc0883d0cc005ffcb1a53aca203529f

morello/dlstart: fix pointer to int conversion
The second parameter to __builtin_cheri_address_set() is a size_t, not a
pointer.

Use the AUX_VAL macro to directly pick the data value from the AUX
stack.

Fixes: e0d77a0b0881 ("Morello: Some refactoring to dlstart.c")
Change-Id: I8ab7f5f3c734458ebdaac3c989960a34d05b0dc3

ldso/dlstart.c
@@ -137,7 +137,8 @@ hidden void _dlstart_c(uintptr_t *sp, size_t *dynv_raw)
 	base_rx = AUX_PTR(aux[AT_BASE]);
 #ifdef __CHERI_PURE_CAPABILITY__
 	if (is_interpreter)
-		base_rx = __builtin_cheri_address_set(rx_cap, base_rx);
+		base_rx = __builtin_cheri_address_set(rx_cap,
+						      AUX_VAL(aux[AT_BASE]));
 #endif
 
 	if (!base_rx) {
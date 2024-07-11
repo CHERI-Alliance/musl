0a0c4ba47af3c0ae2675a3a42b8223f6506cb3d9
Fathi Boudra <fathi.boudra@linaro.org>
2024-07-11 18:37:44 +0200

    [Morello] build-morello.sh: revert CMAKE_ASM_FLAGS to STRING
    ASM_FLAGS should be a string, not a filepath.
    
    Signed-off-by: Fathi Boudra <fathi.boudra@linaro.org>

--- tools/build-morello.sh
+++ tools/build-morello.sh
@@ -193,7 +193,7 @@ set(CMAKE_OBJDUMP "${MORELLO_LLVM_PATH}/bin/llvm-objdump" CACHE FILEPATH "" FORC
 set(CMAKE_OBJCOPY "${MORELLO_LLVM_PATH}/bin/llvm-objcopy" CACHE FILEPATH "" FORCE)
 
 set(LLVM_CONFIG_PATH "${MORELLO_LLVM_PATH}/bin/llvm-config" CACHE FILEPATH "" FORCE)
-set(CMAKE_ASM_FLAGS "-nostdinc -isystem ${SYSROOT}/include" CACHE FILEPATH "" FORCE)
+set(CMAKE_ASM_FLAGS "-nostdinc -isystem ${SYSROOT}/include" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS_DEBUG "-nostdinc -O1 -isystem ${SYSROOT}/include" CACHE FILEPATH "" FORCE)
 set(CMAKE_C_FLAGS_RELEASE "-nostdinc -O3 -DNDEBUG -isystem ${SYSROOT}/include" CACHE FILEPATH "" FORCE)
 EOF

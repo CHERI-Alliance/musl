86e7298f2d1e02a95ad88c5402ea730ae97a77b6
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2024-09-12 10:06:26 +0200

    [BAKEWELL] Do not rely on provided value for argc
    Both on ARM and on RISCV argc for a newly started program is
    passed in the same register that is also used for the system
    call return value. This is fragile and known to be broken in
    at least some kernel version.
    
    Resort to the value provided in the aux vector if present and
    only use the register value as a fallback.

--- crt/crt1.c
+++ crt/crt1.c
@@ -12,6 +12,13 @@ int __libc_start_main(int (*)(), int, char **, char **, auxv_entry *);
 #if defined(__CHERI_PURE_CAPABILITY__)
 void _start_c(intptr_t *p, void *x, int argc, char *argv[], char *envp[], auxv_entry *auxv)
 {
+	/* Prefer argc from aux vector. The register value is not reliable. */
+	for (unsigned int i = 0; auxv[i].a_type != AT_NULL; ++i) {
+		if (auxv[i].a_type == AT_ARGC) {
+			argc = auxv[i].a_un.a_val;
+			break;
+		}
+	}
 	__libc_start_main(main, argc, argv, envp, auxv);
 }
 #else

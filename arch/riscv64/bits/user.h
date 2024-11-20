9fed43d0ff14b878047a2789feb834023a7b5c65
Christian Ehrhardt <christian.ehrhardt@codasip.com>
2024-11-20 18:03:30 +0100

    cheri: Fix elf_gregset_t definition for cheri

--- arch/riscv64/bits/user.h
+++ arch/riscv64/bits/user.h
@@ -1,6 +1,12 @@
 #include <signal.h>
 
+#ifdef __CHERI__
+#define ELF_NGREG 33
+typedef __uintcap_t elf_greg_t, elf_gregset_t[ELF_NGREG];
+#else
 #define ELF_NGREG 32
-#define ELF_NFPREG 33
 typedef unsigned long elf_greg_t, elf_gregset_t[ELF_NGREG];
+#endif
+#define ELF_NFPREG 33
+
 typedef union __riscv_mc_fp_state elf_fpregset_t;

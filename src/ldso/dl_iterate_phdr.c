46ba37ab1b32557ace33badbe5e6daaa4b8b7e38
Petr Vesely <petr.vesely@codasip.com>
2025-02-18 12:00:54 +0000

    [MUSL] Fix linking statically with libunwind

--- src/ldso/dl_iterate_phdr.c
+++ src/ldso/dl_iterate_phdr.c
@@ -8,6 +8,7 @@ extern weak hidden const size_t _DYNAMIC[];
 static int static_dl_iterate_phdr(int(*callback)(struct dl_phdr_info *info, size_t size, void *data), void *data)
 {
 	void *aux_at_phdr;
+	void *rxcap;
 	ElfW(Phdr) *phdr, *tls_phdr=0;
 	uintptr_t base = 0;
 	size_t n;
@@ -27,6 +28,9 @@ static int static_dl_iterate_phdr(int(*callback)(struct dl_phdr_info *info, size
 		case AT_PHNUM:
 			aux_at_phnum = libc.auxv[i].a_un.a_val;
 			break;
+		case AT_CHERI_EXEC_RX_CAP:
+			rxcap = libc.auxv[i].a_un.a_ptr;
+			break;
 		}
 	}
 
@@ -43,6 +47,17 @@ static int static_dl_iterate_phdr(int(*callback)(struct dl_phdr_info *info, size
 		 */
 		phdr = (ElfW(Phdr) *)((char *)phdr + aux_at_phent);
 	}
+
+#if defined(__CHERI_PURE_CAPABILITY__)
+	// For statically linked C++ programs libunwind needs a capability which covers
+	// the entire executable. With the base of 0, this is generally not possible due 
+	// to capability compression. Instead we ensure that we leave 'base' as untagged 0,
+	// and provide dlpi_phdr which spans the executable mapping to use instead.
+	if(base == 0 && !__builtin_cheri_tag_get((void *)base)){
+		aux_at_phdr = __builtin_cheri_address_set(rxcap, (size_t)aux_at_phdr);
+	}
+#endif
+
 	info.dlpi_addr  = base;
 	info.dlpi_name  = "/proc/self/exe";
 	info.dlpi_phdr  = aux_at_phdr;

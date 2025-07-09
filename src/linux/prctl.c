0f4f31d026dc6d29f1a9c82d728c0aef813d0480
Florian Schmaus <florian.schmaus@codasip.com>
2025-07-09 14:19:21 +0200

    prctl: Make prctl(...) a macro for CHERI
    Make prctl() a macro to facilitate prctl() call sites which different
    number of arguments. This is basically the same that was done for
    syscall() in 55e370f7f041 ("cheri: Make syscall(...) a macro for
    CHERI").

--- src/linux/prctl.c
+++ src/linux/prctl.c
@@ -3,13 +3,19 @@
 #include "cheri_helpers.h"
 #include "syscall.h"
 
+#ifndef __CHERI_PURE_CAPABILITY__
+typedef unsigned long prctl_arg_t;
 int prctl(int op, ...)
+#else
+typedef uintptr_t prctl_arg_t;
+int __real_prctl(int op, ...)
+#endif
 {
-	uintptr_t x[4];
+	prctl_arg_t x[4];
 	int i;
 	va_list ap;
 	va_start(ap, op);
-	for (i=0; i<4; i++) x[i] = VA_ARG_IF_IN_BOUNDS(ap, uintptr_t);
+	for (i=0; i<4; i++) x[i] = VA_ARG_IF_IN_BOUNDS(ap, prctl_arg_t);
 	va_end(ap);
 	return syscall(SYS_prctl, op, x[0], x[1], x[2], x[3]);
 }

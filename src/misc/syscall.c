CHERI-Alliance/musl

commit 55e370f7f0418879fabf0be3d5d8b3f2dd6104f6

cheri: Make syscall(...) a macro for CHERI
For CHERI convert the syscall(...) function into a macro
(renaming the actual function to __real_syscall(...)).
Use the macro to force cast all syscall arguments to intptr_t
to avoid issues with var-args on CHERI.

src/misc/syscall.c
@@ -6,7 +6,7 @@
 
 #undef syscall
 
-intptr_t syscall(long n, ...)
+intptr_t __real_syscall(long n, ...)
 {
 	va_list ap;
 	syscall_arg_t a,b,c,d,e,f;

include/unistd.h
@@ -177,7 +177,19 @@ void setusershell(void);
 void endusershell(void);
 char *getusershell(void);
 int acct(const char *);
+#ifndef __CHERI_PURE_CAPABILITY__
 intptr_t syscall(long, ...);
+#define __real_syscall syscall
+#else
+intptr_t __real_syscall(long, ...);
+#define __chericast_sc(X) ((intptr_t)(X))
+#define __chericast_syscall(nr, a, b, c, d, e, f, ...) \
+	__real_syscall(__chericast_sc(nr), __chericast_sc(a), \
+		__chericast_sc(b), __chericast_sc(c), __chericast_sc(d), \
+		__chericast_sc(e), __chericast_sc(f))
+#define syscall(nr, args...) __chericast_syscall(nr, ##args, \
+		0, 0, 0, 0, 0, 0, 0)
+#endif
 int execvpe(const char *, char *const [], char *const []);
 int issetugid(void);
 int getentropy(void *, size_t);

commit 62c710397af78d1697db8c5ab0e3d2514eee9750

syscall: provide 'syscall' symbol if !CHERI_PURE_CAPABILITY
Since 55e370f7f041 ("cheri: Make syscall(...) a macro for CHERI") we
no longer provide a 'syscall' symbol in the non-CHERI_PURE_CAPABILITY
case. Re-add the symbol in this case.

Fixes: 55e370f7f041 ("cheri: Make syscall(...) a macro for CHERI")

src/misc/syscall.c
@@ -6,7 +6,11 @@
 
 #undef syscall
 
+#ifndef __CHERI_PURE_CAPABILITY__
+intptr_t syscall(long n, ...)
+#else
 intptr_t __real_syscall(long n, ...)
+#endif
 {
 	va_list ap;
 	syscall_arg_t a,b,c,d,e,f;
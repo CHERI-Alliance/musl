9e54c11b819a655c8512534fd802c1ecb286e9c8
Stuart Menefy <stuart.menefy@codasip.com>
2025-11-04 18:11:11 +0000

    elf.h: Add Elf{32,64}_Relr
    When SHT_RELR was added in commit d32dadd60efb ("ldso: support
    DT_RELR relative relocation format") the types Elf{32,64}_Relr were
    not added. However the equivalent change to glibc's elf.h did add
    them, and we're now seeing other packages (eg elfutils' libelf.h)
    assuming that if SHT_RELR is defined, Elf{32,64}_Relr will be as
    well.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

--- include/elf.h
+++ include/elf.h
@@ -557,6 +557,9 @@ typedef struct {
 } Elf64_Rela;
 
 
+typedef Elf32_Word	Elf32_Relr;
+typedef Elf64_Xword	Elf64_Relr;
+
 
 #define ELF32_R_SYM(val)		((val) >> 8)
 #define ELF32_R_TYPE(val)		((val) & 0xff)

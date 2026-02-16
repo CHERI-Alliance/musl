5dd39a118af467f4cdb0859aa1d9c1e6e4f45b0d
Chris Hofer <christian.hofer@codasip.com>
2026-02-16 15:15:51 +0100

    malloc: Cherify malloc_usable_size
    For CHERI, have to expand the bounds to retrieve the meta-data of the
    allocator. Note, that this also verifies that the capability that was
    passed in is the one handed out by malloc/realloc...
    
    Signed-off-by: Chris Hofer <christian.hofer@codasip.com>

--- src/malloc/mallocng/malloc_usable_size.c
+++ src/malloc/mallocng/malloc_usable_size.c
@@ -4,6 +4,11 @@
 size_t malloc_usable_size(void *p)
 {
 	if (!p) return 0;
+#ifdef __CHERI_PURE_CAPABILITY__
+	rdlock();
+	p = expand_bounds(p);
+	unlock();
+#endif
 	struct meta *g = get_meta(p);
 	int idx = get_slot_index(p);
 	size_t stride = get_stride(g);

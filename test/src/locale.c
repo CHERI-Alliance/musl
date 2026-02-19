4c05477f59ba79c3084437ca29ce262630e2c189
Florian Schmaus <florian.schmaus@codasip.com>
2026-02-19 11:24:18 +0100

    test: locale: adjust test for updated bind_textdomain_codeset()
    With cabbd8697d39 ("bind_textdomain_codeset: fix return value") musl
    changed the behavior of bind_textdomain_codeset(). Adjust the test
    accordingly.
    
    Also extend the test to check for the expected errno value and update
    the used domainname.

--- test/src/locale.c
+++ test/src/locale.c
@@ -1,5 +1,7 @@
 #include <libintl.h>
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 static int test_bind_textdomain_codeset(void);
 static int test_textdomain(void);
@@ -18,21 +20,41 @@ int main(int argc, char *argv[])
 
 static int test_bind_textdomain_codeset(void)
 {
-	char *res = bind_textdomain_codeset("morello-project.org", 0);
-	printf("res = %s\n", res);
-	if (res != 0) {
+	char *res = bind_textdomain_codeset("cheri-alliance.org", 0);
+	if (!res || strcmp(res, "UTF-8")) {
+		fprintf(stderr,
+				"bind_textdomain_codeset(\"cheri-alliance.org\", 0): expected 'UTF-8', actual %s\n",
+				res ? res : "NULL");
 		return 1;
 	}
-	res = bind_textdomain_codeset("morello-project.org", "utf-8");
-	printf("res = %s\n", res);
-	if (res != 0) {
+	res = bind_textdomain_codeset("cheri-alliance.org", "utf-8");
+	if (!res || strcmp(res, "UTF-8")) {
+		fprintf(stderr,
+				"bind_textdomain_codeset(\"cheri-alliance.org\", \"utf-8\"): expected 'UTF-8', actual %s\n",
+				res ? res : "NULL");
 		return 1;
 	}
-	res = bind_textdomain_codeset("morello-project.org", 0);
-	printf("res = %s\n", res);
-	if (res != 0) {
+	res = bind_textdomain_codeset("cheri-alliance.org", 0);
+	if (!res || strcmp(res, "UTF-8")) {
+		fprintf(stderr,
+				"bind_textdomain_codeset(\"cheri-alliance.org\", 0): expected 'UTF-8', actual %s\n",
+				res ? res : "NULL");
 		return 1;
 	}
+	res = bind_textdomain_codeset("cheri-alliance.org", "C");
+	if (res) {
+		fprintf(stderr,
+				"bind_textdomain_codeset(\"cheri-alliance.org\", \"c\"): expected NULL, actual %s\n",
+				res);
+		return 1;
+	}
+	if (errno != EINVAL) {
+		fprintf(stderr,
+				"bind_textdomain_codeset(\"cheri-alliance.org\", \"c\"): expected errno=EINVAL, actual %d\n",
+				errno);
+		return 1;
+	}
+
 	return 0;
 }
 
@@ -43,7 +65,7 @@ static int test_textdomain(void)
 		return 1;
 	}
 	printf("res = %s\n", res);
-	res = textdomain("morello-project.org");
+	res = textdomain("cheri-alliance.org");
 	printf("res = %s\n", res);
 	if (__builtin_cheri_tag_get(res) == 0) {
 		return 1;

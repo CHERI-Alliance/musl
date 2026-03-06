CHERI-Alliance/musl

commit f4df611c5fe44488dceb0709abc9cdd001ec62bf

Morello: Hashmap-based allocator implementation
This includes:
- A hashmap to map user pointers (narrow bounds) to their group structs,
   which can be used to widen the bounds on their capabilities and
   access the in-band metadata etc. Currently this is likely not the
   most performant implementation, but it can be improved in the
   future without any changes to malloc itself.
- Support for setting correct bounds on allocations on Morello, and
   making sure that the allocations are aligned correctly for these
   bounds. Alignment functionality is now built into malloc (through
   malloc_aligned, as alignment is now required for normal allocations
   and not just aligned_alloc - which now utilised malloc_aligned).
- Support for realloc without using memcpy where possible. Malloc + free
   with memcpy is still used as fallback where increasing the allocation
   size with realloc causes the Morello alignment requirement of the
   user pointer to increase.
- Improvements to allocator tests.

Change-Id: Ie41d06d1de2056c09701b22c59a56018ecd9848f

src/malloc/mallocng/malloc.c
@@ -50,6 +50,11 @@ struct meta *alloc_meta(void)
 #ifndef PAGESIZE
 		ctx.pagesize = get_page_size();
 #endif
+
+#ifdef MORELLO
+		mallocmap_create(1024, &(ctx.capmap));
+#endif
+
 		ctx.secret = get_random_secret();
 		ctx.init_done = 1;
 	}
@@ -269,7 +274,7 @@ static struct meta *alloc_group(int sc, size_t req)
 			return 0;
 		}
 		struct meta *g = ctx.active[j];
-		p = enframe(g, idx, UNIT*size_classes[j]-IB, ctx.mmap_counter);
+		p = enframe(g, idx, UNIT*size_classes[j]-IB, ctx.mmap_counter, UNIT);
 		m->maplen = 0;
 		p[-3] = (p[-3]&31) | (6<<5);
 		for (int i=0; i<=cnt; i++)
@@ -301,7 +306,7 @@ static int alloc_slot(int sc, size_t req)
 	return 0;
 }
 
-void *malloc(size_t n)
+void *malloc_aligned(size_t n, size_t align)
 {
 	if (size_overflows(n)) return 0;
 	struct meta *g;
@@ -310,8 +315,13 @@ void *malloc(size_t n)
 	int idx;
 	int ctr;
 
-	if (n >= MMAP_THRESHOLD) {
-		size_t needed = n + IB + UNIT;
+	size_t padded_n = n;
+	if (align > UNIT) {
+		padded_n += align - UNIT;
+	}
+
+	if (padded_n >= MMAP_THRESHOLD) {
+		size_t needed = padded_n + IB;
 		void *p = mmap(0, needed, PROT_READ|PROT_WRITE,
 			MAP_PRIVATE|MAP_ANON, -1, 0);
 		if (p==MAP_FAILED) return 0;
@@ -337,7 +347,7 @@ void *malloc(size_t n)
 		goto success;
 	}
 
-	sc = size_to_class(n);
+	sc = size_to_class(padded_n);
 
 	rdlock();
 	g = ctx.active[sc];
@@ -371,7 +381,7 @@ void *malloc(size_t n)
 	}
 	upgradelock();
 
-	idx = alloc_slot(sc, n);
+	idx = alloc_slot(sc, padded_n);
 	if (idx < 0) {
 		unlock();
 		return 0;
@@ -380,17 +390,45 @@ void *malloc(size_t n)
 
 success:
 	ctr = ctx.mmap_counter;
-	unlock();
+
 #ifdef MORELLO
-	return __builtin_cheri_bounds_set(enframe(g, idx, n, ctr), n);
-#else
-	return enframe(g, idx, n, ctr);
+	void *p = __builtin_cheri_bounds_set(enframe(g, idx, n, ctr, align), n);
+
+	int r = mallocmap_insert(p, g->mem, &(ctx.capmap));
+	if (!r) {
+		return 0;
+	}
 #endif
+
+	unlock();
+
+#ifdef MORELLO
+	return p;
+#else
+	return enframe(g, idx, n, ctr, align);
+#endif
+}
+
+void *malloc(size_t n) {
+#ifdef MORELLO
+	n = __builtin_cheri_round_representable_length(n);
+	size_t align = ~__builtin_cheri_representable_alignment_mask(n) + 1;
+	if (align < UNIT) align = UNIT;
+#else
+	size_t align = UNIT;
+#endif
+
+	return malloc_aligned(n, align);
 }
 
 int is_allzero(void *p)
 {
-	p = __expand_ddc(p);
+#ifdef MORELLO
+	rdlock();
+	p = expand_bounds(p);
+	unlock();
+#endif
+
 	struct meta *g = get_meta(p);
 	return g->sizeclass >= 48 ||
 		get_stride(g) < UNIT*size_classes[g->sizeclass];

test/src/malloc.c
@@ -1,47 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define test_success 0
-#define unexpected_value 1
-#define null_capability 2
-#define capability_tag_cleared 3
-#define incorrect_permission 4
-#define incorrect_bound 5
-#define bad_alignment 6
-#define bad_morello_alignment 7
-#define bad_test_number 10
-
-#include "caplength.h"
-
-void testptr_size(void* ptr, size_t alloc_size)
-{
-    if(ptr == NULL){
-        exit(null_capability);
-    }
-    if(!__builtin_cheri_tag_get(ptr)){
-        exit(capability_tag_cleared);
-    }
-    unsigned long perm = __builtin_cheri_perms_get(ptr);
-    unsigned long minimal_perm =
-        __CHERI_CAP_PERMISSION_PERMIT_LOAD__ |
-        __CHERI_CAP_PERMISSION_PERMIT_STORE__ |
-        __CHERI_CAP_PERMISSION_PERMIT_STORE_CAPABILITY__ |
-        __CHERI_CAP_PERMISSION_PERMIT_LOAD_CAPABILITY__;
-
-    if((perm & minimal_perm) != minimal_perm){
-        exit(incorrect_permission);
-    }
-
-    int key_map_offset = 0;
-    size_t morello_size = WOULD_BE_LENGTH(alloc_size + key_map_offset, ptr);
-    if(alloc_size != -1 && __builtin_cheri_length_get(ptr) != morello_size){
-        exit(incorrect_bound);
-    }
-}
-
-void testptr(void* ptr){
-    testptr_size(ptr,-1);
-}
+#include "alloc_helpers.h"
 
 #define max_malloc 300 //min 33. Mainly used to size test #3
 
@@ -53,23 +13,24 @@ int main(int argc, char **argv) {
         // test allocating seveal slot of the same size
         all_ptr[0] = malloc(150);
         all_ptr[1] = malloc(150);
-        testptr_size(all_ptr[0],150);
-        testptr(all_ptr[1]);
+        testptr(all_ptr[0], 150);
 
         // test the limit of a slot's size
         all_ptr[2] = malloc(511);
-        testptr(all_ptr[2]);
+        testptr(all_ptr[2], 511);
 
         // test a dedicated mmap
         all_ptr[3] = malloc(32768);
-        testptr(all_ptr[3]);
+        testptr(all_ptr[3], 32768);
 
         break;
     case '1'://basic test for free
         all_ptr[0] = malloc(150);
+        testptr(all_ptr[0], 150);
         //printf("malloc return : %p\n",all_ptr[0]);
         free(all_ptr[0]);
         all_ptr[1] = malloc(16*32768);
+        testptr(all_ptr[1], 16*32768);
         //printf("malloc return : %p\n",all_ptr[0]);
         free(all_ptr[1]);
         break;
@@ -77,18 +38,21 @@ int main(int argc, char **argv) {
     case '2':// tries to fill a few group, make some space, refill. In case there is some corruption of the metadata on free
         for (int i = 0; i < max_malloc; i++){
             all_ptr[i] = malloc(63);
+            testptr(all_ptr[i], 63);
         }
         for (int i = 0; i < max_malloc; i+=2){
             free(all_ptr[i]);
         }
         for (int i = 0; i < max_malloc; i+=2){
             all_ptr[i] = malloc(63);
+            testptr(all_ptr[i], 63);
         }
         break;
     case '3':// Check that the data don't overlaps with the metadata or some other data
         // allocate all slots
         for (int i = 0; i < 33; i++){
             all_ptr[i] = malloc(63);
+            testptr(all_ptr[i], 63);
         }
         // fill them with two set of data, in case some corruption match one set
         for (int i = 0; i < 33; i++){
@@ -103,7 +67,7 @@ int main(int argc, char **argv) {
                 char expected = (j%2 ? j : -j); //I have to put this value in a variable. Making a direct comparison fails.
                 if (val != expected){
                     printf("Some byte seems corrupted. Value : [%hhd, %#02X], j : %d, expected : %d\n", val, val, j, expected);
-                    return unexpected_value;
+                    return UNEXPECTED_VALUE;
                 }
             }
         }
@@ -119,19 +83,21 @@ int main(int argc, char **argv) {
                 char expected = (j%2 ? j : -j);
                 if (val != expected){
                     printf("Some byte seems corrupted after some free(). Value : [%hhd, %#02X], j : %d, expected : %d\n", val, val, j, expected);
-                    return unexpected_value;
+                    return UNEXPECTED_VALUE;
                 }
             }
         }
         break;
     case '4'://segfault on use after free
         all_ptr[0] = malloc(150);
+        testptr(all_ptr[0], 150);
         free(all_ptr[0]);
         *(int*)(all_ptr[0]) = 42;
 
         break;
     case '5'://segfault on double free
         all_ptr[0] = malloc(16*32768);
+        testptr(all_ptr[0], 16*32768);
         free(all_ptr[0]);
         free(all_ptr[0]);
 
@@ -140,7 +106,7 @@ int main(int argc, char **argv) {
         //and not be able to mess with the metadata, not with other adjacent slots. Currently this narrow bound mechanism is not implemented
         //because free need the wide capability to work. Some mechanism have to be added to find the wide capability from the narrowed one.
     default:
-        return bad_test_number;
+        return BAD_TEST_NUMBER;
     }
-    return test_success;
+    return TEST_SUCCESS;
 }

arch/morello/morello.objects
@@ -270,9 +270,9 @@ obj/src/malloc/free.o
 obj/src/malloc/libc_calloc.o
 obj/src/malloc/mallocng/aligned_alloc.o
 obj/src/malloc/mallocng/donate.o
-obj/src/malloc/mallocng/expand.o
 obj/src/malloc/mallocng/free.o
 obj/src/malloc/mallocng/malloc.o
+obj/src/malloc/mallocng/mallocmap.o
 obj/src/malloc/mallocng/malloc_usable_size.o
 obj/src/malloc/mallocng/realloc.o
 obj/src/malloc/memalign.o

src/malloc/mallocng/aligned_alloc.c
@@ -21,41 +21,18 @@ void *aligned_alloc(size_t align, size_t len)
 
 	if (align <= UNIT) align = UNIT;
 
-	unsigned char *p = __expand_ddc(malloc(len + align - UNIT));
-	struct meta *g = get_meta(p);
-	int idx = get_slot_index(p);
-	size_t stride = get_stride(g);
-	unsigned char *start = g->mem->storage + stride*idx;
-	unsigned char *end = g->mem->storage + stride*(idx+1) - IB;
-	size_t adj = -(uintptr_t)p & (align-1);
-
-	if (!adj) {
-		set_size(p, end, len);
-		return p;
-	}
-	p += adj;
-	uint32_t offset = (size_t)(p-g->mem->storage)/UNIT;
-	if (offset <= 0xffff) {
-		*(uint16_t *)(p-2) = offset;
-		p[-4] = 0;
-	} else {
-		// use a 32-bit offset if 16-bit doesn't fit. for this,
-		// 16-bit field must be zero, [-4] byte nonzero.
-		*(uint16_t *)(p-2) = 0;
-		*(uint32_t *)(p-8) = offset;
-		p[-4] = 1;
-	}
-	p[-3] = idx;
-	set_size(p, end, len);
-	// store offset to aligned enframing. this facilitates cycling
-	// offset and also iteration of heap for debugging/measurement.
-	// for extreme overalignment it won't fit but these are classless
-	// allocations anyway.
-	*(uint16_t *)(start - 2) = (size_t)(p-start)/UNIT;
-	start[-3] = 7<<5;
 #ifdef MORELLO
-	return __builtin_cheri_bounds_set(p, len);
-#else
-	return p;
+	size_t morello_len = __builtin_cheri_round_representable_length(len);
+	size_t morello_alignment = ~__builtin_cheri_representable_alignment_mask(morello_len) + 1;
+
+	// If morello alignment is larger, malloc is already going to align this to a more strict alignment
+	//  anyway. We can essentially do nothing.
+	if (morello_alignment >= align) {
+		return malloc(len);
+	}
+
+	len = morello_len;
 #endif
+
+	return malloc_aligned(len, align);
 }

src/malloc/mallocng/expand.S
@@ -1,19 +1,0 @@
-#define FUN(fn) \
-    .global fn; \
-    .balign 16; \
-    .type fn, %function; \
-    fn
-
-#define END(fn) \
-    .size fn, .-fn;
-
-#ifdef MORELLO
-FUN(__expand_ddc):
-    CVTD c0, x0
-    ret
-END(__expand_ddc)
-#else
-FUN(__expand_ddc):
-    ret
-END(__expand_ddc)
-#endif

src/malloc/mallocng/free.c
@@ -101,7 +101,14 @@ static struct mapinfo nontrivial_free(struct meta *g, int i)
 void free(void *p)
 {
 	if (!p) return;
-	p = __expand_ddc((void *)p);
+
+#ifdef MORELLO
+	wrlock();
+	p = expand_bounds(p);
+	mallocmap_delete(p, &(ctx.capmap));
+	unlock();
+#endif
+
 	struct meta *g = get_meta(p);
 	int idx = get_slot_index(p);
 	size_t stride = get_stride(g);

src/malloc/mallocng/glue.h
@@ -96,4 +96,6 @@ static inline void malloc_atfork(int who)
 	else unlock();
 }
 
+void *malloc_aligned(size_t n, size_t align);
+
 #endif

src/malloc/mallocng/mallocmap.c
@@ -1,0 +1,245 @@
+#ifdef MORELLO
+
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <stddef.h>
+
+#include "mallocmap.h"
+
+
+/*
+Open addressing hash table with 2^n table size
+
+Quadratic probing is used in case of hash collision
+
+Tab indices and hash are size_t
+
+Only the key's address is used for calculating the hash - the
+ capaility is simply preserved for validation against the provided
+ capability once the entry is found.
+
+Lazy deletion is used to avoid overly expensive deletions.
+
+There are some special values that can be used for key:
+ - When an element is not present (never been inserted or deleted),
+    NOT_PRESENT (= NULL) is used.
+ - When an element has been deleted, the key is preserved but the
+    value is set to NOT_PRESENT (= NULL).
+
+// TODO: Potentially use Robin Hood hashing for better performance.
+*/
+
+#define NOT_PRESENT ((void *) NULL)
+
+#define MINSIZE 8
+#define MAXSIZE ((size_t)-1/2 + 1)
+
+inline static size_t __attribute__((naked)) mallocmap_keyhash(void *k)
+{
+	__asm__ volatile(
+		"mov     w1, #42\n"
+		"crc32cx w0, w1, x0\n"
+		"ret"
+	);
+}
+
+static int mallocmap_resize(size_t nel, struct __mallocmap_tab *htab)
+{
+	size_t newsize;
+	size_t i, j;
+	MALLOCMAP_ENTRY *e, *newe;
+	MALLOCMAP_ENTRY *oldtab = htab->entries;
+	MALLOCMAP_ENTRY *oldend = htab->entries + htab->mask + 1;
+
+	if (nel > MAXSIZE) nel = MAXSIZE;
+
+	// Ensure newsize is a power of 2, and greater than MINSIZE.
+	for (newsize = MINSIZE; newsize < nel; newsize *= 2);
+
+	// Reset tombstones as we are rebuilding the hash table
+	htab->tombs = 0;
+
+	// Round up new size to nearest 4096 for mmap.
+	size_t map_size = ((newsize * (sizeof *htab->entries)) + 4096 - 1) & -4096;
+
+	htab->entries = mmap(0, map_size,
+		PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, -1, 0);
+
+	if (htab->entries == MAP_FAILED) {
+		htab->entries = oldtab;
+		return 0;
+	}
+
+	htab->mask = newsize - 1;
+
+	// If this is the initial setup, return now.
+	if (!oldtab) return 1;
+
+	// Now that our table has been resized, we must repopulate it
+	//  with new hashes.
+	// TODO: Avoidable with prime number magic?
+	for (e = oldtab; e < oldend; e++) {
+		if (e->key != NOT_PRESENT && e->data != NOT_PRESENT) {
+			// Insert into new hashmap
+			for (i = mallocmap_keyhash(e->key), j=1; ; i += j++) {
+				newe = htab->entries + (i & htab->mask);
+				if (newe->key == NOT_PRESENT)
+					break;
+			}
+
+			newe->key = e->key;
+			newe->data = e->data;
+		}
+	}
+
+	if (oldtab) {
+		// Now that the old data has been copied into the new hash table, we
+		//  can unmap the old table.
+		map_size = ((oldend - oldtab) + 4096 - 1) & -4096;
+		munmap(oldtab, map_size);
+	}
+
+	return 1;
+}
+
+int mallocmap_create(size_t nel, struct __mallocmap_tab *htab)
+{
+	int r = mallocmap_resize(nel, htab);
+
+	return r;
+}
+
+/*
+ * Returns a value for the given key.
+ *
+ * Returns NULL if no value exists.
+ */
+void *mallocmap_find(void *key, struct __mallocmap_tab *htab) {
+	size_t hash = mallocmap_keyhash(key);
+
+	MALLOCMAP_ENTRY *e;
+	MALLOCMAP_ENTRY *tomb = 0;
+	size_t probe = 1;
+
+	while ((e = htab->entries + (hash & htab->mask))->key != key) {
+		if (e->key == NOT_PRESENT) {
+			return NULL;
+		}
+
+		if (!tomb && e->data == NOT_PRESENT) {
+			// Remember this tombstone so we can shift it with our found value.
+			tomb = e;
+		}
+
+		// Quadratic probing.
+		hash += probe++;
+	}
+
+	if (tomb) {
+		// Overwrite the tomb to improve time on any future searches
+		tomb->key = key;
+		tomb->data = e->data;
+
+		e->data = NULL;
+
+		e = tomb;
+	}
+
+	if(!__builtin_cheri_equal_exact(key, e->key)) {
+		// The provided capability is different to when it was inserted.
+		return NULL;
+	}
+
+	return e->data;
+}
+
+/*
+ * Inserts a new value.
+ *
+ * Returns 1 on success, 0 on failure.
+ */
+int mallocmap_insert(void *key, void *data, struct __mallocmap_tab *htab) {
+	size_t hash = mallocmap_keyhash(key);
+
+	MALLOCMAP_ENTRY *e;
+	size_t probe = 1;
+
+	while ((e = htab->entries + (hash & htab->mask))) {
+		if (e->key == NOT_PRESENT) {
+			// New insertion
+			break;
+		}
+
+		if (e->data == NOT_PRESENT) {
+			// Replace tombstone
+			e->key = key;
+			e->data = data;
+			htab->tombs--;
+
+			return 1;
+		}
+
+		if (e->key == key) {
+			// Key already exists in map
+			return 0;
+		}
+
+		// Quadratic probing
+		hash += probe++;
+	}
+
+	// Insert new entry into hash table.
+	e->key = key;
+	e->data = data;
+
+	// Double hashmap size if our hashmap is too full now.
+	if (++htab->used > htab->mask - htab->mask/4) {
+		if (!mallocmap_resize(2 * htab->used, htab)) {
+			// Resize failed, return error.
+			htab->used--;
+			e->key = NOT_PRESENT;
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+/*
+ * Delete item from hashmap.
+ *
+ * Returns 1 on success, 0 on failure.
+ */
+int mallocmap_delete(void *key, struct __mallocmap_tab *htab) {
+	size_t hash = mallocmap_keyhash(key);
+
+	MALLOCMAP_ENTRY *e;
+
+	size_t probe = 1;
+	size_t index = hash;
+	while ((e = htab->entries + (index & htab->mask))->key != key) {
+		if (e->key == NOT_PRESENT) {
+			return 0;
+		}
+
+		// Quadratic probing
+		index += probe++;
+	}
+
+	e->data = NOT_PRESENT;
+
+	htab->used--;
+	if (++htab->tombs > htab->used && htab->used > htab->mask / 4) {
+		// Rebuild the hash table if the number of tombs exceeds number of
+		//  active items.
+		if (!mallocmap_resize(2 * htab->used, htab)) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+#endif

commit 8c71570de1e58f78c435de1739cb9e7ff99175b4

malloc: ensure that malloc lock is unlocked if mallocmap_insert failed
If mallocmap_insert() fails, currently only possible due mmap()
failing when resizing the map, then we should not return NULL without
releasing the malloc lock.

Fix this by adding a call to unlock() before returning.

Also avoid leaking the allocated slot by setting its bit in freed_mask
again. Note that we can not set the bit in the avail_mask as it could
lead to an inconsistent state where the group becomes unreachable.

Thanks to Stuart Menefy for pointing out the potential memory leak in
the first iterations of this patch.

Fixes: f4df611c5fe4 ("Morello: Hashmap-based allocator implementation")

src/malloc/mallocng/malloc.c
@@ -439,6 +439,22 @@ success:
 
 	int r = mallocmap_insert(p, g->mem, &(ctx.capmap));
 	if (!r) {
+		// set the freed bit. we either hold the rdlock or the wrlock
+		// here. in any case, g will stay the active group and only be
+		// potentially replaced after we release the lock and a wrlock()
+		// is aquired subsequently. in a subsequent alloc path, freed_mask
+		// bits will be transferred to avail_mask. or, in the free path,
+		// the group will be potentially destroyed.
+		first = 1u<<idx;
+		if (MT) {
+			for (;;) {
+				mask = g->freed_mask;
+				if (a_cas(&g->freed_mask,mask,mask+first)==mask) break;
+			}
+		} else
+			g->freed_mask += first;
+
+		unlock();
 		return 0;
 	}
 #endif